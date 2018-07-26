75bec1b7e331e124a60d630d4bb9b710d1786c72
mrstephenneal <stephen.neal14@gmail.com>
2018-07-26 11:18:45 -0400

    Utility functions importable via *

---
+++ pdfwatermarker/utils/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['set_destination', 'resource_path', 'add_suffix', 'open_window', 'write_pdf']
+
+
+from pdfwatermarker.utils.path import set_destination, resource_path, add_suffix
+from pdfwatermarker.utils.view import open_window
+from pdfwatermarker.utils.write import write_pdf
