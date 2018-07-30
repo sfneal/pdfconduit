af7cd4190c00b6db99d3fa4f5407cc927a716919
mrstephenneal <stephen.neal14@gmail.com>
2018-07-30 13:23:00 -0400

    Added PyPDF2 upscale method to allow adding margins while upscaling

--- pdfwatermarker/upscale.py
+++ pdfwatermarker/upscale.py
@@ -1,23 +1,58 @@
 # Upscale a PDF file
 from pdfrw import PdfReader, PdfWriter, PageMerge, IndirectPdfDict
-from pdfwatermarker import set_destination
+from pdfwatermarker.thirdparty.PyPDF2 import PdfFileReader, PdfFileWriter
+from pdfwatermarker.thirdparty.PyPDF2.pdf import PageObject
+from pdfwatermarker import set_destination, get_pdf_size
 
 
-def upscale(file_name, margin=0, scale=1.5):
+def upscale(file_name, margin=0, margin_x=0, margin_y=0, scale=1.5, method='pypdf2'):
     """Upscale a PDF to a large size."""
-    def adjust(page):
-        info = PageMerge().add(page)
-        x1, y1, x2, y2 = info.xobj_box
-        viewrect = (margin, margin, x2 - x1 - 2 * margin, y2 - y1 - 2 * margin)
-        page = PageMerge().add(page, viewrect=viewrect)
-        page[0].scale(scale)
-        return page.render()
-
+    # Output file name
     output = set_destination(file_name, 'upscaled')
-    reader = PdfReader(file_name)
-    writer = PdfWriter(output)
-    for i in list(range(0, len(reader.pages))):
-        writer.addpage(adjust(reader.pages[i]))
-    writer.trailer.Info = IndirectPdfDict(reader.Info or {})
-    writer.write()
+
+    def pdfrw():
+        def adjust(page):
+            info = PageMerge().add(page)
+            x1, y1, x2, y2 = info.xobj_box
+            print(info.xobj_box)
+            viewrect = (margin, margin, x2 - x1 - 2 * margin, y2 - y1 - 2 * margin)
+            print(viewrect)
+            page = PageMerge().add(page, viewrect=viewrect)
+            page[0].scale(scale)
+            return page.render()
+
+        reader = PdfReader(file_name)
+        writer = PdfWriter(output)
+        for i in list(range(0, len(reader.pages))):
+            writer.addpage(adjust(reader.pages[i]))
+        writer.trailer.Info = IndirectPdfDict(reader.Info or {})
+        writer.write()
+
+    def pypdf2():
+        reader = PdfFileReader(file_name)
+        writer = PdfFileWriter()
+
+        dims = get_pdf_size(file_name)
+        target_w = dims['w'] * scale
+        target_h = dims['h'] * scale
+
+        # Number of pages in input document
+        page_count = reader.getNumPages()
+
+        for page_number in range(page_count):
+            wtrmrk = reader.getPage(page_number)
+
+            page = PageObject.createBlankPage(width=target_w, height=target_h)
+            page.mergeScaledTranslatedPage(wtrmrk, scale, margin_x, margin_y)
+            writer.addPage(page)
+
+        with open(output, "wb") as outputStream:
+            writer.write(outputStream)
+        return output
+
+    if method is 'pypdf2':
+        pypdf2()
+    else:
+        pdfrw()
+    print(get_pdf_size(output))
     return output
