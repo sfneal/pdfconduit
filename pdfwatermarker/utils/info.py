7b15c207d6bda8d9e15fd1c8617c4cf0df502ecb
mrstephenneal <stephen.neal14@gmail.com>
2018-08-01 09:29:22 -0400

    Added _reader function to decrypt encrypted PDF

--- pdfwatermarker/utils/info.py
+++ pdfwatermarker/utils/info.py
@@ -2,42 +2,54 @@
 from pdfwatermarker.thirdparty.PyPDF2 import PdfFileReader
 
 
-def number_of_pages(path):
-    """Retrieve PDF number of pages"""
-    return PdfFileReader(path).getNumPages()
-
-
-def metadata(path):
-    """Retrieve PDF metadata"""
-    return PdfFileReader(path).getDocumentInfo()
-
-
-def resources(path):
-    """Retrieve contents of each page of PDF"""
+def _reader(path, password):
+    """Read PDF and decrypt if encrypted."""
     pdf = PdfFileReader(path)
+    # Check that PDF is encrypted
+    if pdf.isEncrypted:
+        # Check that password is none
+        if not password:
+            pdf.decrypt('')
+            # Try and decrypt PDF using no password, prompt for password
+            if pdf.isEncrypted:
+                print('No password has been given for encrypted PDF ', path)
+                password = input('Enter Password: ')
+        pdf.decrypt(password)
+    return pdf
+
+
+def number_of_pages(path, password=None):
+    """Retrieve PDF number of pages"""
+    return _reader(path, password).getNumPages()
+
+
+def metadata(path, password=None):
+    """Retrieve PDF metadata"""
+    return _reader(path, password).getDocumentInfo()
+
+
+def resources(path, password=None):
+    """Retrieve contents of each page of PDF"""
+    pdf = _reader(path, password)
     return [pdf.getPage(i) for i in range(pdf.getNumPages)]
 
 
-def text_extractor(path):
+def text_extractor(path, password=None):
     """Extract text from a PDF file"""
-    pdf = PdfFileReader(path)
+    pdf = _reader(path, password)
     return [pdf.getPage(i).extractText() for i in range(PdfFileReader(path).getNumPages)]
 
 
-def security_objects(path, password='baz'):
+def security_objects(path, password=None):
     """Print security object information for a pdf document"""
-    pdf = PdfFileReader(path)
-    pdf.decrypt(password)
-    for k in pdf.resolvedObjects.items():
-        print(k[0])
-        for i, v in k[1].items():
-            print(i, '-->', v)
+    pdf = _reader(path, password)
+    return [(k, v) for i in pdf.resolvedObjects.items() for k, v in i[1].items()]
 
 
-def get_pdf_size(file_name):
+def get_pdf_size(file_name, password=None):
     """Get width and height of a PDF"""
     try:
-        size = PdfFileReader(file_name).getPage(0).mediaBox
+        size = _reader(file_name, password).getPage(0).mediaBox
     except AttributeError:
         size = file_name.getPage(0).mediaBox
     return {'w': float(size[2]), 'h': float(size[3])}
--- tests/test_watermark_secure.py
+++ tests/test_watermark_secure.py
@@ -1,22 +1,29 @@
 import os
-from pdfwatermarker import Watermark, EncryptParams, add_suffix, open_window
+from pdfwatermarker import Watermark, EncryptParams, add_suffix, open_window, security_objects, metadata, number_of_pages
 from looptools import ActiveTimer
 
 
 def main():
     print('Testing Watermark class and secure function reliability')
-    directory = '/Users/Stephen/Desktop'
-    pdf = os.path.join(directory, '20180015(20170238) CDS(layered).pdf')
 
+    directory = '/Users/Stephen/Desktop'
+    filename = '20150094_Market Model.pdf'
+
+    pdf = os.path.join(directory, filename)
     project = '20160054'
     address = '43 Indian Lane'
     town = 'Franklin'
     state = 'MA'
 
-    enc = EncryptParams('baz', 'foo', output=add_suffix(pdf, 'secured'))
+    user_pw = 'baz'
+    owner_pw = 'foo'
 
+    enc = EncryptParams(user_pw, owner_pw, output=add_suffix(pdf, 'secured'))
     with ActiveTimer(Watermark):
-        Watermark(pdf, project, address, town, state, encrypt=enc, encrypt_128=True)
+        secured = Watermark(pdf, project, address, town, state, encrypt=enc, encrypt_128=True)
+
+    print(metadata(str(secured), user_pw))
+    print(security_objects(str(secured), user_pw))
     print('Success!')
 
 
