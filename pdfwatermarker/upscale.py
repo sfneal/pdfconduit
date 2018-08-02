87bfcb48312c91eab3f26bf3db445d415d0ad8d0
mrstephenneal <stephen.neal14@gmail.com>
2018-08-02 12:00:46 -0400

    Tempfile integration

--- pdfwatermarker/upscale.py
+++ pdfwatermarker/upscale.py
@@ -1,14 +1,15 @@
 # Upscale a PDF file
+from tempfile import NamedTemporaryFile
 from pdfrw import PdfReader, PdfWriter, PageMerge, IndirectPdfDict
 from pdfwatermarker.thirdparty.PyPDF2 import PdfFileReader, PdfFileWriter
 from pdfwatermarker.thirdparty.PyPDF2.pdf import PageObject
 from pdfwatermarker import set_destination, info
 
 
-def upscale(file_name, margin=0, margin_x=0, margin_y=0, scale=1.5, method='pypdf2'):
+def upscale(file_name, margin=0, margin_x=0, margin_y=0, scale=1.5, method='pypdf2', tempdir=None):
     """Upscale a PDF to a large size."""
     # Output file name
-    output = set_destination(file_name, 'upscaled')
+    output = NamedTemporaryFile(suffix='.pdf', dir=tempdir)
 
     def pdfrw():
         def adjust(page):
