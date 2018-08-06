db7e8c005ecd9bcdcb7ac2126ec43aad58ed6d80
mrstephenneal <stephen.neal14@gmail.com>
2018-08-06 12:35:34 -0400

    Changed bundle_dir from string import to function import

--- tests/test_create_img.py
+++ tests/test_create_img.py
@@ -1,12 +1,25 @@
-from pdfwatermarker.watermark.canvas import pil_create_letter, pil_draw_text_centered, pil_save
+import os
+import shutil
+from pdfwatermarker.watermark.watermark import Watermark
 from tests import directory, pdf
 
 
 def main():
-    img = pil_create_letter()
-    img = pil_draw_text_centered(img, 'Hello World')
-    i = pil_save(img, directory)
-    print(i)
+    print('Testing Watermark class flatten reliability')
+
+    address = '43 Indian Lane'
+    town = 'Franklin'
+    state = 'MA'
+
+    w = Watermark(pdf, remove_temps=True, use_receipt=False)
+    flat = w.draw(address, str(town + ', ' + state), opacity=0.08, flatten=True)
+    layered = w.draw(address, str(town + ', ' + state), opacity=0.08, flatten=False)
+    shutil.move(flat, os.path.join(directory, 'flat.pdf'))
+    shutil.move(layered, os.path.join(directory, 'layered.pdf'))
+    wm = w.cleanup()
+
+    if os.path.exists(wm):
+        print('Success!')
 
 
 if __name__ == '__main__':
