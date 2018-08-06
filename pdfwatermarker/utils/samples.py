994f97011f30c4b47f86893048c53cc0267ced48
mrstephenneal <stephen.neal14@gmail.com>
2018-08-06 16:01:15 -0400

    Added layering function to samples.py that creates a flat vs. layered comparison pdf

--- pdfwatermarker/utils/samples.py
+++ pdfwatermarker/utils/samples.py
@@ -1,9 +1,9 @@
 # Generate sample PDF documents
 import os
 from pdfwatermarker import Watermark, open_window, Merge, slicer, info
-from pdfwatermarker.watermark.draw import available_images, CanvasObjects, CanvasStr, WatermarkDraw
-from pdfwatermarker.utils.gui import get_file
-from tqdm import tqdm
+from pdfwatermarker.watermark.draw import available_images
+from pdfwatermarker.watermark.watermark import Label
+from tests import pdf
 
 
 def watermarks(destination, images=available_images()):
@@ -15,61 +15,69 @@ def watermarks(destination, images=available_images()):
                     copyright=False)
         watermarks.append(wm)
     m = Merge(watermarks, 'Watermarks samples', destination)
-    return w, m
+    return m.file, w
 
 
-def opacity(source):
-    _source = source
+def opacity(source, dst):
     samples = []
     _range = range(4, 25)[::3]
-    wm = Watermark(source, use_receipt=False, open_file=False, remove_temps=False)
+    wm = Watermark(source, use_receipt=False, open_file=False, remove_temps=True)
     if info.pages_count(source) > 2:
         source = slicer(source, 1, 1, wm.tempdir)
-    for i in tqdm(_range):
+    for i in _range:
         o = i * .01
         wtrmrk = wm.draw(text1='200 Stonewall Blvd', text2='Wrentham, MA', opacity=o)
         watermarked = wm.add(document=source, watermark=wtrmrk, output='temp')
 
-        objects = CanvasObjects()
-        objects.add(CanvasStr(str(str(i).zfill(2) + '%'), opacity=1, x=15, y=30, x_centered=False))
-        label = WatermarkDraw(objects, tempdir=wm.tempdir).write()
-
-        wtrmrk_with_label = wm.add(watermarked, label, output='temp')
-        samples.append(wtrmrk_with_label)
-    m = Merge(samples, 'Opacity comparison samples', os.path.dirname(_source))
-    return wm, m
+        labeled_pdf = Label(watermarked, str(str(i).zfill(2) + '%'), tempdir=wm.tempdir).write()
+        samples.append(labeled_pdf)
+    m = Merge(samples, 'Opacity comparison samples', dst)
+    return m.file, wm
 
 
-def placement(source):
-    _source = source
+def placement(source, dst):
     wm = Watermark(source, use_receipt=False, open_file=False, remove_temps=True)
     if info.pages_count(source) > 2:
         source = slicer(source, 1, 1, wm.tempdir)
     wtrmrk = wm.draw(text1='200 Stonewall Blvd',
                      text2='Wrentham, MA')
     over = wm.add(document=source, watermark=wtrmrk, output='temp', underneath=False)
-    objects = CanvasObjects()
-    objects.add(CanvasStr('Overlayed', size=14, opacity=1, x=15, y=30, x_centered=False))
-    label = WatermarkDraw(objects, tempdir=wm.tempdir).write()
-    over_with_label = wm.add(over, label, output='temp')
+    over_with_label = Label(over, 'Overlay', tempdir=wm.tempdir).write()
 
     under = wm.add(document=source, watermark=wtrmrk, output='temp', underneath=True)
-    objects = CanvasObjects()
-    objects.add(CanvasStr('Underneath', size=14, opacity=1, x=15, y=30, x_centered=False))
-    label = WatermarkDraw(objects, tempdir=wm.tempdir).write()
-    under_with_label = wm.add(under, label, output='temp')
-    m = Merge([over_with_label, under_with_label], 'Watermark Placement samples', os.path.dirname(_source))
-    return wm, m
+    under_with_label = Label(under, 'Underneath', tempdir=wm.tempdir).write()
+
+    m = Merge([over_with_label, under_with_label], 'Watermark Placement samples', dst)
+    return m.file, wm
+
+
+def layering(source, dst):
+    wm = Watermark(source, use_receipt=False, open_file=False, remove_temps=True)
+    if info.pages_count(source) > 2:
+        source = slicer(source, 1, 1, wm.tempdir)
+    flat = wm.draw(text1='200 Stonewall Blvd', text2='Wrentham, MA', flatten=True)
+    layered = wm.draw(text1='200 Stonewall Blvd', text2='Wrentham, MA', flatten=False)
+
+    wtrmrked_flat = Label(wm.add(document=source, watermark=flat, output='temp'), 'Flat', tempdir=wm.tempdir).write()
+    wtrmrked_layer = Label(wm.add(document=source, watermark=layered, output='temp'), 'Layered', tempdir=wm.tempdir).write()
+
+    watermark_flat = Label(flat, 'Flat', tempdir=wm.tempdir).write()
+    watermark_layer = Label(layered, 'Layered', tempdir=wm.tempdir).write()
+    m = Merge([wtrmrked_flat, watermark_flat, wtrmrked_layer, watermark_layer], 'Layering samples', dst)
+    return m, wm
 
 
 def main():
-    src = get_file()
-    dst = os.path.dirname(src)
-    wm, m = watermarks(dst)
-    wm, m = opacity(src)
-    wm, m = placement(src)
+    src = pdf
+    dst = os.path.join(os.path.dirname(src), 'samples')
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+    m, wm = watermarks(dst)
+    m, wm = opacity(src, dst)
+    m, wm = placement(src, dst)
+    m, wm = layering(src, dst)
 
-    wm.cleanup(receipt=False)
+    wm.cleanup()
     open_window(m)
 
 
