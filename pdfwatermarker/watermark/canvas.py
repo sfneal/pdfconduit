9fbb074748b45eb46336add2c823e74eb9b05b52
mrstephenneal <stephen.neal14@gmail.com>
2018-08-06 12:35:04 -0400

    Created DrawPIL class to draw text to an image instead of PDF canvas

--- pdfwatermarker/watermark/canvas.py
+++ pdfwatermarker/watermark/canvas.py
@@ -2,7 +2,7 @@ import os
 from tempfile import NamedTemporaryFile
 from reportlab.lib.pagesizes import letter
 from PIL import Image, ImageEnhance, ImageDraw, ImageFont
-from pdfwatermarker.watermark.utils import FONT
+from pdfwatermarker.watermark.utils import FONT, resource_path, bundle_dir
 
 
 class CanvasStr:
@@ -45,7 +45,7 @@ class CanvasObjects:
         self.objects.append(canvas_object)
 
 
-def img_opacity(image, opacity, tempdir=None):
+def img_opacity(image, opacity, tempdir=None, bw=True):
     """
     Returns an image with reduced opacity.
     Taken from http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/362879
@@ -60,42 +60,54 @@ def img_opacity(image, opacity, tempdir=None):
     alpha = im.split()[3]
     alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
     im.putalpha(alpha)
+    if bw:
+        im.convert('L')
     im.save(dst)
     return dst
 
 
-def center_text(text, page_width, drawing, font):
+def centered_text(text, page_width, drawing, font):
     text_width = drawing.textsize(text, font=font)
     s = (page_width[0] - text_width[0]) / 2
-    print(s)
     return s
 
 
-def pil_save(img, destination, file_name='letter'):
-    # Save image file
-    output = os.path.join(destination, file_name.strip('.png') + '.png')
-    img.save(output)
-    return output
+class DrawPIL:
+    def __init__(self, size=(792, 612)):
+        # Create a black image
+        self.img = Image.new('RGBA', size, color=(255, 255, 255, 0))  # 2200, 1700 for 200 DPI
 
+    def _centered_text(self, text, drawing, font):
+        # Get img size
+        page_width = self.img.size
+        text_width = drawing.textsize(text, font=font)
+        x = (page_width[0] - text_width[0]) / 2
+        return x
 
-def pil_create_letter(size=(792, 612)):
-    # Create a black image
-    img = Image.new('RGBA', size, color=(255, 255, 255, 0))  # 2200, 1700 for 200 DPI
-    return img
+    def draw_text(self, text, x='center', y=140, font=FONT, size=40, opacity=0.1):
+        # Set drawing context
+        d = ImageDraw.Draw(self.img)
 
+        # Set a font
+        fnt = ImageFont.truetype(font, int(size * 1.25))
 
-def pil_draw_text_centered(img, text, y=140, font=FONT, size=40, opacity=0.1):
-    # Get img size
-    img_size = img.size
+        # Check if x is set to 'center'
+        if 'center' in str(x).lower():
+            x = self._centered_text(text, d, fnt)
 
-    # Set drawing context
-    d = ImageDraw.Draw(img)
+        # Draw text to image
+        d.text((x, y), text, font=fnt, fill=(0, 0, 0, int(255 / (opacity * 100))))
 
-    # Set a font
-    fnt = ImageFont.truetype(font, size)
+    def save(self, destination=None, file_name='pil', tempdir=None):
+        fn = file_name.strip('.png') if '.png' in file_name else file_name
+        if tempdir:
+            tmpimg = NamedTemporaryFile(suffix='.png', dir=tempdir, delete=False)
+            output = resource_path(tmpimg.name)
+        elif destination:
+            output = os.path.join(destination, fn + '.png')
+        else:
+            output = os.path.join(bundle_dir(), fn + '.png')
 
-    # Draw text to image
-    s = center_text(text, img_size, d, fnt)
-    d.text((s, y), text, font=fnt, fill=(0, 0, 0, int(255 / (opacity * 100))))
-    return img
-
+        # Save image file
+        self.img.save(output)
+        return output
