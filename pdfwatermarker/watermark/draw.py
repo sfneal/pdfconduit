ba9bd669f2726a11410f5e150af4954a7d8feb8d
mrstephenneal <stephen.neal14@gmail.com>
2018-08-06 09:35:49 -0400

    Moved canvas classes to canvas.py

--- pdfwatermarker/watermark/draw.py
+++ pdfwatermarker/watermark/draw.py
@@ -10,6 +10,7 @@ from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from pdfwatermarker.utils import resource_path, write_pdf
+from pdfwatermarker.watermark.canvas import CanvasImg, CanvasObjects, CanvasStr
 
 
 def bundle_dir():
@@ -65,46 +66,6 @@ def img_opacity(image, opacity, tempdir=None):
     im.putalpha(alpha)
     im.save(dst)
     return dst
-
-
-class CanvasStr:
-    """Canvas string data object used for storing canvas.drawString parameters."""
-    def __init__(self, string, font='Vera', color='black', size=40, opacity=0.1, x=None, y=None,
-                 x_centered=True):
-        self.string = string
-        self.font = font
-        self.color = color
-        self.size = size
-        self.opacity = opacity
-        self.x = x
-        self.y = y
-        self.x_centered = x_centered
-
-
-class CanvasImg:
-    """Canvas image data object used for storing canvas.drawImage parameters."""
-    def __init__(self, image, opacity=0.1, x=0, y=0, w=letter[0], h=letter[1], mask='auto',
-                 preserve_aspect_ratio=True):
-        self.image = image
-        self.opacity = opacity
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.mask = mask
-        self.preserve_aspect_ratio = preserve_aspect_ratio
-
-
-class CanvasObjects:
-    """Canvas object collector to store list of canvas objects."""
-    def __init__(self):
-        self.objects = []
-
-    def __iter__(self):
-        return iter(self.objects)
-
-    def add(self, canvas_object):
-        self.objects.append(canvas_object)
 
 
 class Draw:
