6af9bb4dfc0fa0ae20a31ad6a757adf73cdffe47
mrstephenneal <stephen.neal14@gmail.com>
2018-08-07 10:30:14 -0400

    Added WatermarkAdd and Label to watermark module __init__.py

--- pdfwatermarker/watermark/__init__.py
+++ pdfwatermarker/watermark/__init__.py
@@ -1,4 +1,6 @@
-__all__ = ["Watermark", "WatermarkGUI"]
+__all__ = ["Watermark", "WatermarkGUI", "WatermarkAdd", "Label"]
 
 
 from pdfwatermarker.watermark.watermark import Watermark, WatermarkGUI
+from pdfwatermarker.watermark.add import WatermarkAdd
+from pdfwatermarker.watermark.label import Label
