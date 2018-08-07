cc1804401ee04f5083e267226a03343e58a9ae6f
mrstephenneal <stephen.neal14@gmail.com>
2018-08-07 16:59:08 -0400

    Added assertion to confirm that input pages is equal to output pages

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,31 +1,21 @@
-from pdfwatermarker import Merge
 import os
-import shutil
+from pdfwatermarker import Merge, info
+from tests import directory
 
 
 def main():
-    print('Testing merge function reliability')
-    hpa = '/Volumes/Storage/HPA Design/Marketing Library/Floor Plan PDFs'
-    desktop = '/Users/Stephen/Desktop'
-    pdfs = ['20110167_FP.1.pdf', '20110167_FP.2.pdf', '20110167_FP.3.pdf']
-
-    hpa_paths = [os.path.join(hpa, pdf) for pdf in pdfs]
-    pdf_paths = [os.path.join(desktop, pdf) for pdf in pdfs]
-    for f in pdf_paths:
-        try:
-            os.remove(f)
-        except FileNotFoundError:
-            pass
-
-    for f in hpa_paths:
-        shutil.copy2(f, desktop)
-
-    # output pdf file name
-    output = 'combined_example.pdf'
+    print('Testing Merge reliability')
+    pdfs = [os.path.join(directory, p) for p in ['article.pdf', 'charts.pdf', 'document.pdf', 'manual.pdf']]
 
     # Merge PDF files
-    Merge(pdf_paths, output_name=output)
-    print('Done!')
+    m = Merge(pdfs)
+
+    try:
+        assert os.path.exists(m.file) is True
+        assert sum([info.pages(pdf) for pdf in pdfs]) == info.pages(m.file)
+        print('Success!')
+    except AssertionError:
+        print('Failed!')
 
 
 if __name__ == "__main__":
