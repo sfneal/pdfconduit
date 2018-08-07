426b0619c6e3ce81c8a1f8ee76f16eddc317ffa0
mrstephenneal <stephen.neal14@gmail.com>
2018-08-07 16:44:31 -0400

    Added file comparison assertion to check if flat and layered files are different

--- tests/test_watermark_flat.py
+++ tests/test_watermark_flat.py
@@ -1,12 +1,11 @@
 import os
-import shutil
-from pdfwatermarker.watermark.watermark import Watermark
-from tests import directory, pdf
+from pdfwatermarker import Watermark
+from tests import pdf
+import filecmp
 
 
 def main():
-    print('Testing Watermark class flatten reliability')
-
+    print('Testing Watermark draw, add and encrypt functionality')
     address = '43 Indian Lane'
     town = 'Franklin'
     state = 'MA'
@@ -14,12 +13,16 @@ def main():
     w = Watermark(pdf, remove_temps=True, use_receipt=False)
     flat = w.draw(address, str(town + ', ' + state), opacity=0.08, flatten=True)
     layered = w.draw(address, str(town + ', ' + state), opacity=0.08, flatten=False)
-    shutil.move(flat, os.path.join(directory, 'wm flat.pdf'))
-    shutil.move(layered, os.path.join(directory, 'wm layered.pdf'))
-    wm = w.cleanup()
 
-    if os.path.exists(wm):
+    try:
+        # File checks
+        assert os.path.exists(flat) is True
+        assert os.path.exists(layered) is True
+        assert filecmp.cmp(flat, layered) is False
+        w.cleanup()
         print('Success!')
+    except AssertionError:
+        print('Failed!')
 
 
 if __name__ == '__main__':
