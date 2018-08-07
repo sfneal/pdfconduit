a57c76bb2b780e829180f82c948de12f29deeaca
mrstephenneal <stephen.neal14@gmail.com>
2018-08-07 10:31:47 -0400

    Added WatermarkAdd and Label to pdfwatermarker module __init__.py

--- pdfwatermarker/__init__.py
+++ pdfwatermarker/__init__.py
@@ -1,10 +1,10 @@
-__all__ = ["upscale", "rotate", "protect", "Merge", "Watermark", "WatermarkGUI", "slicer"]
+__all__ = ["upscale", "rotate", "protect", "Merge", "Watermark", "WatermarkGUI", "Label", "WatermarkAdd", "slicer"]
 __version__ = '1.0.3'
 __author__ = 'Stephen Neal'
 
 
 from pdfwatermarker.utils import *
-from pdfwatermarker.watermark import Watermark, WatermarkGUI
+from pdfwatermarker.watermark import Watermark, WatermarkGUI, Label, WatermarkAdd
 from pdfwatermarker.upscale import upscale
 from pdfwatermarker.rotate import rotate
 from pdfwatermarker.encrypt import protect
