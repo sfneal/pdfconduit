95e22be083ca9f4efac09d6b0f07c384447025c9
mrstephenneal <stephen.neal14@gmail.com>
2018-08-08 13:00:00 -0400

    Added batch watermarking to GUI.watermark() method

--- pdfwatermarker/utils/gui.py
+++ pdfwatermarker/utils/gui.py
@@ -1,3 +1,4 @@
+import os
 import PySimpleGUI as gui
 from pdfwatermarker import __version__
 from pdfwatermarker.watermark.draw.canvas import available_images
@@ -88,106 +89,166 @@ class GUI:
         from pdfwatermarker.watermark import Watermark
         receipt = Receipt()
 
-        def form():
-            title = 'PDF Watermarker'
-            """GUI window for inputing Watermark parameters"""
-            label_w = 20
-            with gui.FlexForm(title, auto_size_text=True, default_element_size=(40, 1)) as form:
-                layout = [
-                    [gui.Text('HPA Design', size=(30, 1), font=("Helvetica", 25), text_color='blue')],
+        label_w = 20
+        title = 'PDF Watermarker'
+
+        def header():
+            return [[gui.Text('HPA Design', size=(30, 1), font=("Helvetica", 25), text_color='blue')],
                     [gui.Text('PDF Watermark utility', size=(30, 1), font=("Helvetica", 25), text_color='blue')],
                     [gui.Text('version: ' + __version__, size=(30, 1), font=("Helvetica", 16), text_color='blue')],
+                    [_line()]]
 
-                    [_line()],
+        def footer(message='Click Submit to watermark PDF'):
+            return [[gui.Text(message)], [gui.Submit(), gui.Cancel()]]
 
+        def folder(params):
+            with gui.FlexForm(title, auto_size_text=True, default_element_size=(40, 1)) as form:
+                inputs = [
                     # Source
                     [gui.Text('Source', font=('Helvetica', 15), justification='left')],
-                    [gui.Text('Source PDF file', size=(label_w, 1), auto_size_text=False), gui.InputText('Source'),
-                     gui.FileBrowse(file_types=(("PDF Files", "*.pdf"),))],
+                    [gui.Text('Source folder', size=(label_w, 1), auto_size_text=False),
+                     gui.InputText(params['pdf'], size=(30, 1)),
+                     gui.FolderBrowse(button_text='Folder')],
+
+                    [_line()],
+                ]
+                layout = []
+                layout.extend(header())
+                layout.extend(inputs)
+                layout.extend(footer())
+
+                (button, (values)) = form.LayoutAndShow(layout)
+
+            params['pdf'] = values[0]
+            return params
+
+        def settings(params):
+            # Fix opacity if it is adjusted$
+            if params['opacity'] < 1:
+                params['opacity'] = int(params['opacity'] * 100)
+
+            """GUI window for inputing Watermark parameters"""
+            with gui.FlexForm(title, auto_size_text=True, default_element_size=(40, 1)) as form:
+
+                inputs = [
+                    # Source
+                    [gui.Text('Source', font=('Helvetica', 15), justification='left')],
+                    [gui.Text('Source file or folder', size=(label_w, 1), auto_size_text=False),
+                     gui.InputText(params['pdf'], size=(30, 1)),
+                     gui.FileBrowse(button_text='File', file_types=(("PDF Files", "*.pdf"),)),
+                     gui.SimpleButton('Folder')],
+                    # [gui.Text('Select file to watermark one PDF.  Select folder to batch watermark multiple PDFs.')],
 
                     [_line()],
 
                     # Files and non-empty-folders
                     [gui.Text('Project address', font=('Helvetica', 15), justification='left')],
-                    [gui.Text('Address', size=(label_w, 1), auto_size_text=False), gui.InputText()],
-                    [gui.Text('Town', size=(label_w, 1), auto_size_text=False), gui.InputText()],
-                    [gui.Text('State', size=(label_w, 1), auto_size_text=False), gui.InputText()],
+                    [gui.Text('Address', size=(label_w, 1), auto_size_text=False), gui.InputText(params['address'])],
+                    [gui.Text('Town', size=(label_w, 1), auto_size_text=False), gui.InputText(params['town'])],
+                    [gui.Text('State', size=(label_w, 1), auto_size_text=False), gui.InputText(params['state'])],
 
                     [_line()],
 
                     [gui.Text('Watermark Settings', font=('Helvetica', 15), justification='left')],
                     [gui.Text('Logo Image', size=(label_w, 1), auto_size_text=False),
-                     gui.InputCombo(values=(available_images()), size=(30, 4))],
+                     gui.InputCombo(values=(params['image']), size=(30, 4))],
 
                     [gui.Text('File Compression', size=(label_w, 1), auto_size_text=False),
-                     gui.Radio('Uncompressed', "RADIO1", default=True), gui.Radio('Compressed', "RADIO1")],
+                     gui.Radio('Uncompressed', "RADIO1", default=params['compression']['uncompressed']),
+                     gui.Radio('Compressed', "RADIO1", default=params['compression']['compressed'])],
 
                     [gui.Text('Watermark Flattening', size=(label_w, 1), auto_size_text=False),
-                     gui.Radio('Layered', "RADIO3"), gui.Radio('Flattened', "RADIO3", default=True)],
+                     gui.Radio('Flattened', "RADIO3", default=params['flattening']['flattened']),
+                     gui.Radio('Layered', "RADIO3", default=params['flattening']['layered'])],
                     [gui.Text('Watermark Placement', size=(label_w, 1), auto_size_text=False),
-                     gui.Radio('Overlay', "RADIO2", default=True), gui.Radio('Underneath', "RADIO2")],
+                     gui.Radio('Overlay', "RADIO2", default=params['placement']['overlay']),
+                     gui.Radio('Underneath', "RADIO2", default=params['placement']['underneath'])],
 
                     [gui.Text('Opacity', size=(label_w, 1), auto_size_text=False),
-                     gui.Slider(range=(1, 20), orientation='h', size=(34, 30), default_value=8)],
+                     gui.Slider(range=(1, 20), orientation='h', size=(34, 30), default_value=params['opacity'])],
 
                     [_line()],
 
                     # Encryption
                     [gui.Text('Encryption Settings', font=('Helvetica', 15), justification='left')],
-                    [gui.Checkbox('Encrypt', default=True)],
-                    [gui.Text('User Password', size=(label_w, 1), auto_size_text=False), gui.InputText()],
-                    [gui.Text('Owner Password', size=(label_w, 1), auto_size_text=False), gui.InputText()],
-
-                    [gui.Text('Click Submit to watermark PDF')],
-
-                    [gui.Submit(), gui.Cancel()]
+                    [gui.Checkbox('Encrypt', default=params['encrypt'])],
+                    [gui.Text('User Password', size=(label_w, 1), auto_size_text=False), gui.InputText(params['user_pw'])],
+                    [gui.Text('Owner Password', size=(label_w, 1), auto_size_text=False), gui.InputText(params['owner_pw'])],
                 ]
+                layout = []
+                layout.extend(header())
+                layout.extend(inputs)
+                layout.extend(footer())
 
                 (button, (values)) = form.LayoutAndShow(layout)
 
-            opacity = float(values[11] * .01)
-            user_pw = values[13] if len(values[13]) > 0 else ''
-            owner_pw = values[14] if len(values[14]) > 0 else ''
-            params = {
-                'pdf': values[0],
-                'address': values[1],
-                'town': values[2],
-                'state': values[3],
-                'image': values[4],
-                'compression': {
-                    'uncompressed': values[5],
-                    'compressed': values[6]
-                },
-                'flattening': {
-                    'layered': values[7],
-                    'flattened': values[8],
-                },
-                'placement': {
-                    'overlay': values[9],
-                    'underneath': values[10]
-                },
-                'opacity': opacity,
-                'encrypt': values[12],
-                'user_pw': user_pw,
-                'owner_pw': owner_pw,
-            }
+            params['pdf'] = values[0]
+            params['address'] = values[1]
+            params['town'] = values[2]
+            params['state'] = values[3]
+            params['image'] = values[4]
+            params['compression']['uncompressed'] = values[5]
+            params['compression']['compressed'] = values[6]
+            params['flattening']['flattened'] = values[7]
+            params['flattening']['layered'] = values[8]
+            params['placement']['overlay'] = values[9]
+            params['placement']['underneath'] = values[10]
+            params['opacity'] = float(values[11] * .01)
+            params['encrypt'] = values[12]
+            params['user_pw'] = values[13] if len(values[13]) > 0 else ''
+            params['owner_pw'] = values[14] if len(values[14]) > 0 else ''
+            if button == 'Folder':
+                params = folder(params)
+                params = settings(params)
+
             return params
 
-        params = form()
-        receipt.set_dst(params['pdf'])
+        params = {
+            'pdf': '',
+            'address': '',
+            'town': '',
+            'state': '',
+            'image': available_images(),
+            'compression': {
+                'uncompressed': True,
+                'compressed': False
+            },
+            'flattening': {
+                'flattened': True,
+                'layered': False,
+            },
+            'placement': {
+                'overlay': True,
+                'underneath': False
+            },
+            'opacity': 8,
+            'encrypt': True,
+            'user_pw': '',
+            'owner_pw': '',
+        }
 
-        # Execute Watermark class
-        wm = Watermark(params['pdf'], receipt=receipt)
-        wm.draw(text1=params['address'],
-                text2=str(params['town'] + ', ' + params['state']),
-                image=params['image'],
-                opacity=params['opacity'],
-                compress=params['compression']['compressed'],
-                flatten=params['flattening']['flattened'])
-        wm.add(underneath=params['placement']['underneath'])
+        params = settings(params)
 
-        if params['encrypt']:
-            wm.encrypt(params['user_pw'], params['owner_pw'])
+        if os.path.isfile(params['pdf']):
+            params['pdf'] = [params['pdf']]
+        elif os.path.isdir(params['pdf']):
+            src_dir = params['pdf']
+            params['pdf'] = [os.path.join(src_dir, pdf) for pdf in os.listdir(src_dir) if pdf.endswith('.pdf')]
+
+        receipt.set_dst(params['pdf'][0])
+
+        for pdf in params['pdf']:
+            # Execute Watermark class
+            wm = Watermark(pdf, receipt=receipt)
+            wm.draw(text1=params['address'],
+                    text2=str(params['town'] + ', ' + params['state']),
+                    image=params['image'],
+                    opacity=params['opacity'],
+                    compress=params['compression']['compressed'],
+                    flatten=params['flattening']['flattened'])
+            wm.add(underneath=params['placement']['underneath'])
+            if params['encrypt']:
+                wm.encrypt(params['user_pw'], params['owner_pw'])
         wm.cleanup()
 
         try:
