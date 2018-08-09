3fc378ed4ea0eab968c82664da95a2d2eb7165c4
mrstephenneal <stephen.neal14@gmail.com>
2018-08-09 10:41:11 -0400

    Optimized path functions import

--- pdfwatermarker/watermark/lib/__init__.py
+++ pdfwatermarker/watermark/lib/__init__.py
@@ -1,7 +1,7 @@
-__all__ = ['Receipt', 'bundle_dir', 'register_font', 'FONT', 'LETTER', 'IMAGE_DEFAULT', 'IMAGE_DIRECTORY',
+__all__ = ['Receipt', 'register_font', 'FONT', 'LETTER', 'IMAGE_DEFAULT', 'IMAGE_DIRECTORY',
            'available_images']
 
 
 from pdfwatermarker.watermark.lib.receipt import Receipt
-from pdfwatermarker.watermark.lib.utils import bundle_dir, register_font, FONT, LETTER, IMAGE_DEFAULT, \
+from pdfwatermarker.watermark.lib.utils import register_font, FONT, LETTER, IMAGE_DEFAULT, \
     IMAGE_DIRECTORY, available_images
