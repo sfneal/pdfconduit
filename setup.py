a47ce5ea6526592c1253a6c161f9cbbf38b826d5
mrstephenneal <stephen.neal14@gmail.com>
2018-08-09 13:28:34 -0400

    Updated readme and setup.py

--- setup.py
+++ setup.py
@@ -2,24 +2,16 @@ from setuptools import setup, find_packages
 from pdfconduit import __version__, __author__
 
 long_description = """
-A Pure-Python library built as a PDF toolkit.  It was developed to prepare documents for distribution.
+A Pure-Python library built as a PDF toolkit.  Prepare documents for distribution.
 
-- Watermark
-    - Dynamically generate watermarks
-    - Add watermark to existing document
-- Label
-    - Overlay text labels such as filename or date to documents
-    - Add title page to document 
-- Encrypt
-    - Password protect
-    - Restrict permissions to print only
-- Rotate 
-    - By increments of 90 degrees
-- Upscale
-- Merge
-    - Concatenate multiple documents into one file
-- Slice
-    - Extract page ranges from documents
+Features:
+- Watermark: Dynamically generate watermarks and add watermark to existing document
+- Label: Overlay text labels such as filename or date to documents 
+- Encrypt: Password protect and restrict permissions to print only
+- Rotate: Rotate by increments of 90 degrees
+- Upscale: Scale PDF size
+- Merge: Concatenate multiple documents into one file
+- Slice: Extract page ranges from documents
 - Extract Text and Images
 - Retrieve document metadata and information
 """
