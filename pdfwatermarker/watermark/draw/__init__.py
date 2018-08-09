de367adcde69677c9e5d9e59dae8648cbaf4b29a
mrstephenneal <stephen.neal14@gmail.com>
2018-08-09 10:49:05 -0400

    Optimized imports

--- pdfwatermarker/watermark/draw/__init__.py
+++ pdfwatermarker/watermark/draw/__init__.py
@@ -1,5 +1,5 @@
-__all__ = ['Draw', 'WatermarkDraw', 'DrawPIL', 'img_opacity']
+__all__ = ['Draw', 'WatermarkDraw', 'DrawPIL', 'img_opacity', 'CanvasStr', 'CanvasObjects', 'CanvasImg']
 
 
-from pdfwatermarker.watermark.draw.canvas import Draw, WatermarkDraw
+from pdfwatermarker.watermark.draw.canvas import Draw, WatermarkDraw, CanvasImg, CanvasObjects, CanvasStr
 from pdfwatermarker.watermark.draw.image import DrawPIL, img_opacity
