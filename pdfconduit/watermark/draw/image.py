fd30b0412b90d9587393082e68042c1a1717aa24
mrstephenneal <stephen.neal14@gmail.com>
2018-08-10 14:53:50 -0400

    Additional methods added to DrawPIL

--- pdfconduit/watermark/draw/image.py
+++ pdfconduit/watermark/draw/image.py
@@ -38,20 +38,36 @@ def img_opacity(image, opacity, tempdir=None, bw=True):
     # Save modified image file
     dst = NamedTemporaryFile(suffix='.png', dir=tempdir, delete=False)
     im.save(dst)
-    return dst
+    return dst.name
 
 
 class DrawPIL:
-    def __init__(self, size=(792, 612)):
+    def __init__(self, size=(792, 612), tempdir=None):
         # Create a black image
         self.img = Image.new('RGBA', size, color=(255, 255, 255, 0))  # 2200, 1700 for 200 DPI
+        self.tempdir = tempdir
 
-    def _centered_text(self, text, drawing, font):
+    def _centered_x(self, text, drawing, font):
         # Get img size
-        page_width = self.img.size
-        text_width = drawing.textsize(text, font=font)
-        x = (page_width[0] - text_width[0]) / 2
+        page_width = self.img.size[0]
+        text_width = drawing.textsize(text, font=font)[0]
+        x = (page_width - text_width) / 2
         return x
+
+    def _centered_y(self, size):
+        # Get img size
+        page_height = self.img.size[1]
+        y = (page_height - size) / 2
+        return y
+
+    def _scale(self, img):
+        im = img if isinstance(img, Image.Image) else Image.open(img)
+
+        scale = min(float(self.img.size[0] / im.size[0]), float(self.img.size[1] / im.size[1]))
+
+        im.thumbnail((int(im.size[0] * scale), int(im.size[1] * scale)))
+
+        return im if isinstance(img, Image.Image) else self.save(img=im)
 
     def draw_text(self, text, x='center', y=140, font=FONT, size=40, opacity=0.1):
         # Set drawing context
@@ -62,15 +78,48 @@ class DrawPIL:
 
         # Check if x is set to 'center'
         if 'center' in str(x).lower():
-            x = self._centered_text(text, d, fnt)
+            x = self._centered_x(text, d, fnt)
+
+        # Check if y is set to 'center'
+        if 'center' in str(y).lower():
+            y = self._centered_y(size)
 
         # Draw text to image
         d.text((x, y), text, font=fnt, fill=(0, 0, 0, int(255 / (opacity * 100))))
 
-    def save(self, destination=None, file_name='pil', tempdir=None):
+    def draw_img(self, img, x=0, y=0, opacity=1.0):
+        scaled = self._scale(img)
+
+        # scaled = im.resize(target_width)
+        opacity = Image.open(img_opacity(scaled, opacity, self.tempdir))
+
+        self.img.paste(opacity, (x, y))
+
+    def rotate(self, rotate):
+        # Create transparent image that is the same size as self.img
+        mask = Image.new('L', self.img.size, 255)
+
+        # Rotate image and then scale image to fit self.img
+        front = self._scale(self.img.rotate(rotate, expand=True))
+
+        # Rotate mask
+        self._scale(mask.rotate(rotate, expand=True))
+
+        # Determine difference in size between mask and front
+        x_margin = int((mask.size[0] - front.size[0]) / 2)
+
+        # Create another new image
+        rotated = Image.new('RGBA', self.img.size, color=(255, 255, 255, 0))
+
+        # Paste front into new image and set x offset equal to half the difference of front and mask size
+        rotated.paste(front, (x_margin, 0))
+        self.img = rotated
+
+    def save(self, destination=None, file_name='pil', img=None):
+        img = self.img if not img else img
         fn = file_name.strip('.png') if '.png' in file_name else file_name
-        if tempdir:
-            tmpimg = NamedTemporaryFile(suffix='.png', dir=tempdir, delete=False)
+        if self.tempdir:
+            tmpimg = NamedTemporaryFile(suffix='.png', dir=self.tempdir, delete=False)
             output = resource_path(tmpimg.name)
         elif destination:
             output = os.path.join(destination, fn + '.png')
@@ -78,5 +127,5 @@ class DrawPIL:
             output = os.path.join(bundle_dir(), fn + '.png')
 
         # Save image file
-        self.img.save(output)
+        img.save(output)
         return output
