e3fb35876663fa8326463e1eeb2ed3246d76a089
mrstephenneal <stephen.neal14@gmail.com>
2018-08-10 14:52:57 -0400

    Added centered parameter to CanvasImg object. Added Canvas.translate method call to WatermarkDraw.draw method in order to set canvas origin to middle. Added anchorAtXY parameter to WatermarkDraw._draw_image.

--- pdfconduit/watermark/draw/canvas.py
+++ pdfconduit/watermark/draw/canvas.py
@@ -26,7 +26,7 @@ class CanvasStr:
 class CanvasImg:
     """Canvas image data object used for storing canvas.drawImage parameters."""
     def __init__(self, image, opacity=0.1, x=0, y=0, w=LETTER[0], h=LETTER[1], mask='auto',
-                 preserve_aspect_ratio=True):
+                 preserve_aspect_ratio=True, centered=False):
         self.image = image
         self.opacity = opacity
         self.x = x
@@ -35,6 +35,7 @@ class CanvasImg:
         self.h = h
         self.mask = mask
         self.preserve_aspect_ratio = preserve_aspect_ratio
+        self.centered = centered
 
 
 class CanvasObjects:
@@ -66,7 +67,7 @@ class Draw:
 
         # create a new PDF with Reportlab
         self.packet = io.BytesIO()
-        self.can = Canvas(self.packet, pagesize=LETTER, pageCompression=compress)  # Initialize canvas
+        self.can = Canvas(self.packet, pagesize=LETTER, pageCompression=compress, bottomup=1)  # Initialize canvas
 
     def __str__(self):
         return str(self.dst)
@@ -89,6 +90,9 @@ class WatermarkDraw(Draw):
         self.draw()
 
     def draw(self):
+        # Move canvas origin to the middle of the page
+        self.can.translate(self.can._pagesize[0] / 2, self.can._pagesize[1] / 2)
+
         # Rotate canvas
         self.can.rotate(self.rotate)
 
@@ -103,9 +107,9 @@ class WatermarkDraw(Draw):
     def _draw_image(self, canvas_image):
         """Draw Image to canvas"""
         img = img_opacity(canvas_image.image, canvas_image.opacity, tempdir=self.dir)
-        self.can.drawImage(img.name, x=canvas_image.x, y=canvas_image.y, width=canvas_image.w,
+        self.can.drawImage(img, x=canvas_image.x, y=canvas_image.y, width=canvas_image.w,
                            height=canvas_image.h, mask=canvas_image.mask,
-                           preserveAspectRatio=canvas_image.preserve_aspect_ratio)
+                           preserveAspectRatio=canvas_image.preserve_aspect_ratio, anchorAtXY=True)
 
     def _draw_string(self, canvas_string):
         """Draw string to canvas"""
