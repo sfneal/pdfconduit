8576bfffe197ffd17a35ca88cdf65df3932374fa
mrstephenneal <stephen.neal14@gmail.com>
2018-08-17 09:20:20 -0400

    Corrected imports

--- pdfconduit/utils/__init__.py
+++ pdfconduit/utils/__init__.py
@@ -1,12 +1,12 @@
 __all__ = ['set_destination', 'resource_path', 'add_suffix', 'open_window', 'overlay_pdfs', 'write_pdf', 'info',
            'get_directory', 'get_file', 'bundle_dir', 'Receipt', 'FONT', 'LETTER', 'IMAGE_DEFAULT',
-           'IMAGE_DIRECTORY', 'available_images']
+           'IMAGE_DIRECTORY', 'available_images', 'GUI']
 
 
 from pdfconduit.utils import info
 from pdfconduit.utils.path import set_destination, resource_path, add_suffix, bundle_dir
 from pdfconduit.utils.view import open_window
 from pdfconduit.utils.write import overlay_pdfs, write_pdf
-from pdfconduit.utils.gui import get_directory, get_file
+from pdfconduit.utils.gui import get_directory, get_file, GUI
 from pdfconduit.utils.receipt import Receipt
 from pdfconduit.utils.lib import FONT, LETTER, IMAGE_DIRECTORY, IMAGE_DEFAULT, available_images
