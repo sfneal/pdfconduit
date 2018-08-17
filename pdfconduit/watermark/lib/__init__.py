8fe65c6ad89349fd1261b9afd48ebf12a0276813
mrstephenneal <stephen.neal14@gmail.com>
2018-08-17 09:18:33 -0400

    Moved watermark lib and utils modules to package level utils module

--- pdfconduit/watermark/lib/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['Receipt', 'FONT', 'LETTER', 'IMAGE_DEFAULT', 'IMAGE_DIRECTORY', 'available_images']
-
-
-from pdfconduit.watermark.lib.receipt import Receipt
-from pdfconduit.watermark.lib.utils import register_font, FONT, LETTER, IMAGE_DEFAULT, IMAGE_DIRECTORY, available_images
