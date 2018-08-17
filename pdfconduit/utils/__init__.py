7bfd27c0e650f8877ac170abf92b0d0a473e45c6
mrstephenneal <stephen.neal14@gmail.com>
2018-08-17 11:01:53 -0400

    Modified utils module Info class import

--- pdfconduit/utils/__init__.py
+++ pdfconduit/utils/__init__.py
@@ -1,9 +1,9 @@
-__all__ = ['set_destination', 'resource_path', 'add_suffix', 'open_window', 'overlay_pdfs', 'write_pdf', 'info',
+__all__ = ['set_destination', 'resource_path', 'add_suffix', 'open_window', 'overlay_pdfs', 'write_pdf', 'Info',
            'get_directory', 'get_file', 'bundle_dir', 'Receipt', 'FONT', 'LETTER', 'IMAGE_DEFAULT',
            'IMAGE_DIRECTORY', 'available_images', 'GUI']
 
 
-from pdfconduit.utils import info
+from pdfconduit.utils.info import Info
 from pdfconduit.utils.path import set_destination, resource_path, add_suffix, bundle_dir
 from pdfconduit.utils.view import open_window
 from pdfconduit.utils.write import overlay_pdfs, write_pdf
