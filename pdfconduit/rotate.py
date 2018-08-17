37e79acb92ed9efd45e36339f8565c404683e954
mrstephenneal <stephen.neal14@gmail.com>
2018-08-17 14:08:56 -0400

    Removed pdfrw dependencies

--- pdfconduit/rotate.py
+++ pdfconduit/rotate.py
@@ -3,10 +3,9 @@ import os
 from tempfile import NamedTemporaryFile
 from PyPDF3 import PdfFileReader, PdfFileWriter
 from pdfconduit.utils import add_suffix
-from pdfrw import PdfReader, PdfWriter
 
 
-def rotate(file_name, rotate, method='pypdf3', suffix='rotated', tempdir=None):
+def rotate(file_name, rotate, suffix='rotated', tempdir=None):
     """Rotate PDF by increments of 90 degrees."""
     # Set output file name
     if tempdir:
@@ -16,37 +15,14 @@ def rotate(file_name, rotate, method='pypdf3', suffix='rotated', tempdir=None):
     else:
         outfn = NamedTemporaryFile(suffix='.pdf').name
 
-    def pypdf3():
-        with open(file_name, 'rb') as pdf_in:
-            pdf_writer = PdfFileWriter()
-            pdf_reader = PdfFileReader(pdf_in)
-            for pagenum in range(pdf_reader.numPages):
-                page = pdf_reader.getPage(pagenum)
-                page.rotateClockwise(rotate)
-                pdf_writer.addPage(page)
+    with open(file_name, 'rb') as pdf_in:
+        pdf_writer = PdfFileWriter()
+        pdf_reader = PdfFileReader(pdf_in)
+        for pagenum in range(pdf_reader.numPages):
+            page = pdf_reader.getPage(pagenum)
+            page.rotateClockwise(rotate)
+            pdf_writer.addPage(page)
 
-            with open(outfn, 'wb') as pdf_out:
-                pdf_writer.write(pdf_out)
-        return outfn
-
-    def pdfrw():
-        trailer = PdfReader(file_name)
-        pages = trailer.pages
-
-        ranges = [[1, len(pages)]]
-
-        for onerange in ranges:
-            onerange = (onerange + onerange[-1:])[:2]
-            for pagenum in range(onerange[0] - 1, onerange[1]):
-                pages[pagenum].Rotate = (int(pages[pagenum].inheritable.Rotate or
-                                             0) + rotate) % 360
-
-        outdata = PdfWriter(outfn)
-        outdata.trailer = trailer
-        outdata.write()
-        return outfn
-
-    if method is 'pypdf3':
-        return pypdf3()
-    else:
-        return pdfrw()
+        with open(outfn, 'wb') as pdf_out:
+            pdf_writer.write(pdf_out)
+    return outfn
--- pdfconduit/upscale.py
+++ pdfconduit/upscale.py
@@ -1,13 +1,12 @@
 # Upscale a PDF file
 import os
 from tempfile import NamedTemporaryFile
-from pdfrw import PdfReader, PdfWriter, PageMerge, IndirectPdfDict
 from PyPDF3 import PdfFileReader, PdfFileWriter
 from PyPDF3.pdf import PageObject
 from pdfconduit.utils import Info, add_suffix
 
 
-def upscale(file_name, margin_x=0, margin_y=0, scale=1.5, method='pypdf3', suffix='scaled', tempdir=None):
+def upscale(file_name, margin_x=0, margin_y=0, scale=1.5, suffix='scaled', tempdir=None):
     """Upscale a PDF to a large size."""
     # Set output file name
     if tempdir:
@@ -17,44 +16,22 @@ def upscale(file_name, margin_x=0, margin_y=0, scale=1.5, method='pypdf3', suffi
     else:
         output = NamedTemporaryFile(suffix='.pdf').name
 
-    def pdfrw():
-        def adjust(page):
-            info = PageMerge().add(page)
-            x1, y1, x2, y2 = info.xobj_box
-            viewrect = (margin_x, margin_y, x2 - x1 - 2 * margin_x, y2 - y1 - 2 * margin_y)
-            page = PageMerge().add(page, viewrect=viewrect)
-            page[0].scale(scale)
-            return page.render()
+    reader = PdfFileReader(file_name)
+    writer = PdfFileWriter()
+    dims = Info(file_name).dimensions
+    target_w = dims['w'] * scale
+    target_h = dims['h'] * scale
 
-        reader = PdfReader(file_name)
-        writer = PdfWriter(output)
-        for i in list(range(0, len(reader.pages))):
-            writer.addpage(adjust(reader.pages[i]))
-        writer.trailer.Info = IndirectPdfDict(reader.Info or {})
-        writer.write()
+    # Number of pages in input document
+    page_count = reader.getNumPages()
 
-    def pypdf3():
-        reader = PdfFileReader(file_name)
-        writer = PdfFileWriter()
-        dims = Info(file_name).dimensions
-        target_w = dims['w'] * scale
-        target_h = dims['h'] * scale
+    for page_number in range(page_count):
+        wtrmrk = reader.getPage(page_number)
 
-        # Number of pages in input document
-        page_count = reader.getNumPages()
+        page = PageObject.createBlankPage(width=target_w, height=target_h)
+        page.mergeScaledTranslatedPage(wtrmrk, scale, margin_x, margin_y)
+        writer.addPage(page)
 
-        for page_number in range(page_count):
-            wtrmrk = reader.getPage(page_number)
-
-            page = PageObject.createBlankPage(width=target_w, height=target_h)
-            page.mergeScaledTranslatedPage(wtrmrk, scale, margin_x, margin_y)
-            writer.addPage(page)
-
-        with open(output, "wb") as outputStream:
-            writer.write(outputStream)
-
-    if method is 'pypdf3':
-        pypdf3()
-    else:
-        pdfrw()
+    with open(output, "wb") as outputStream:
+        writer.write(outputStream)
     return output
