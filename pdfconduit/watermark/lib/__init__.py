ac72acab942b3777cc38585a7f821a1ff9112f31
mrstephenneal <stephen.neal14@gmail.com>
2018-08-17 09:11:02 -0400

    Optimized imports by removing unneeded functions from __all__

--- pdfconduit/watermark/lib/__init__.py
+++ pdfconduit/watermark/lib/__init__.py
@@ -1,7 +1,5 @@
-__all__ = ['Receipt', 'register_font', 'FONT', 'LETTER', 'IMAGE_DEFAULT', 'IMAGE_DIRECTORY',
-           'available_images']
+__all__ = ['Receipt', 'FONT', 'LETTER', 'IMAGE_DEFAULT', 'IMAGE_DIRECTORY', 'available_images']
 
 
 from pdfconduit.watermark.lib.receipt import Receipt
-from pdfconduit.watermark.lib.utils import register_font, FONT, LETTER, IMAGE_DEFAULT, \
-    IMAGE_DIRECTORY, available_images
+from pdfconduit.watermark.lib.utils import register_font, FONT, LETTER, IMAGE_DEFAULT, IMAGE_DIRECTORY, available_images
