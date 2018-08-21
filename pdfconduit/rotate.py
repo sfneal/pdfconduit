ce701311cd86c56bba63ef4659728d8b5c314a65
mrstephenneal <stephen.neal14@gmail.com>
2018-08-21 12:19:08 -0400

    Created Rotate class and modifed rotate function to be a wrapper for Rotate

--- pdfconduit/rotate.py
+++ pdfconduit/rotate.py
@@ -6,31 +6,47 @@ from pdfrw import PdfReader, PdfWriter
 from pdfconduit.utils import add_suffix
 
 
-def rotate(file_name, rotation, suffix='rotated', tempdir=None, method='pypdf3'):
-    """Rotate PDF by increments of 90 degrees."""
-    # Set output file name
-    if tempdir:
-        outfn = NamedTemporaryFile(suffix='.pdf', dir=tempdir, delete=False).name
-    elif suffix:
-        outfn = os.path.join(os.path.dirname(file_name), add_suffix(file_name, suffix))
-    else:
-        outfn = NamedTemporaryFile(suffix='.pdf').name
+class Rotate:
+    def __init__(self, file_name, rotation, suffix='rotated', tempdir=None, method='pypdf3'):
+        self.file_name = file_name
+        self.rotation = rotation
+        self.suffix = suffix
+        self.tempdir = tempdir
 
-    def pypdf3():
-        with open(file_name, 'rb') as pdf_in:
+        if tempdir:
+            self.outfn = NamedTemporaryFile(suffix='.pdf', dir=tempdir, delete=False).name
+        elif suffix:
+            self.outfn = os.path.join(os.path.dirname(file_name), add_suffix(file_name, suffix))
+        else:
+            self.outfn = NamedTemporaryFile(suffix='.pdf').name
+
+        if method is 'pypdf3':
+            self.pypdf3()
+        else:
+            self.pdfrw()
+
+    def __str__(self):
+        return self.file
+
+    @property
+    def file(self):
+        return str(self.outfn)
+
+    def pypdf3(self):
+        with open(self.file_name, 'rb') as pdf_in:
             pdf_writer = PdfFileWriter()
             pdf_reader = PdfFileReader(pdf_in)
             for pagenum in range(pdf_reader.numPages):
                 page = pdf_reader.getPage(pagenum)
-                page.rotateClockwise(rotation)
+                page.rotateClockwise(self.rotation)
                 pdf_writer.addPage(page)
 
-            with open(outfn, 'wb') as pdf_out:
+            with open(self.outfn, 'wb') as pdf_out:
                 pdf_writer.write(pdf_out)
-        return outfn
+        return self.outfn
 
-    def pdfrw():
-        trailer = PdfReader(file_name)
+    def pdfrw(self):
+        trailer = PdfReader(self.file_name)
         pages = trailer.pages
 
         ranges = [[1, len(pages)]]
@@ -38,14 +54,14 @@ def rotate(file_name, rotation, suffix='rotated', tempdir=None, method='pypdf3')
         for onerange in ranges:
             onerange = (onerange + onerange[-1:])[:2]
             for pagenum in range(onerange[0] - 1, onerange[1]):
-                pages[pagenum].Rotate = (int(pages[pagenum].inheritable.Rotate or 0) + rotation) % 360
+                pages[pagenum].Rotate = (int(pages[pagenum].inheritable.Rotate or 0) + self.rotation) % 360
 
-        outdata = PdfWriter(outfn)
+        outdata = PdfWriter(self.outfn)
         outdata.trailer = trailer
         outdata.write()
-        return outfn
+        return self.outfn
 
-    if method is 'pypdf3':
-        return pypdf3()
-    else:
-        return pdfrw()
+
+def rotate(file_name, rotation, suffix='rotated', tempdir=None, method='pypdf3'):
+    """Rotate PDF by increments of 90 degrees."""
+    return str(Rotate(file_name, rotation, suffix, tempdir, method))
