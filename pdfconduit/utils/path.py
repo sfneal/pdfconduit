a1ef144dae5f686a6321493bcb871ce9a7d1d96a
mrstephenneal <stephen.neal14@gmail.com>
2018-08-21 16:01:50 -0400

    Fixed file name construction error

--- pdfconduit/utils/path.py
+++ pdfconduit/utils/path.py
@@ -27,8 +27,8 @@ def resource_path(relative):
 if 'pathlib' in sys.modules:
     def _add_suffix(file_path, suffix, sep, ext):
         p = Path(file_path)
-        ext = p.stem if ext is None else ext
-        out = ext + sep + suffix + p.suffix  # p.suffix is file extension
+        _ext = p.suffix if ext is None else str('.' + ext.strip('.'))
+        out = p.stem + sep + suffix + _ext  # p.suffix is file extension
         return os.path.join(os.path.dirname(file_path), out)
 else:
     def _add_suffix(file_path, suffix, sep, ext):
