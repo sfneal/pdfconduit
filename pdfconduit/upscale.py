47f1aba71a25eaccc18a3fb09ddd238618128702
mrstephenneal <stephen.neal14@gmail.com>
2018-08-21 11:08:19 -0400

    Restored pdfrw method for speed improvements

--- pdfconduit/upscale.py
+++ pdfconduit/upscale.py
@@ -3,10 +3,11 @@ import os
 from tempfile import NamedTemporaryFile
 from PyPDF3 import PdfFileReader, PdfFileWriter
 from PyPDF3.pdf import PageObject
+from pdfrw import PdfReader, PdfWriter, PageMerge, IndirectPdfDict
 from pdfconduit.utils import Info, add_suffix
 
 
-def upscale(file_name, margin_x=0, margin_y=0, scale=1.5, suffix='scaled', tempdir=None):
+def upscale(file_name, margin_x=0, margin_y=0, scale=1.5, suffix='scaled', tempdir=None, method='pypdf3'):
     """Upscale a PDF to a large size."""
     # Set output file name
     if tempdir:
@@ -16,22 +17,45 @@ def upscale(file_name, margin_x=0, margin_y=0, scale=1.5, suffix='scaled', tempd
     else:
         output = NamedTemporaryFile(suffix='.pdf').name
 
-    reader = PdfFileReader(file_name)
-    writer = PdfFileWriter()
-    dims = Info(file_name).dimensions
-    target_w = dims['w'] * scale
-    target_h = dims['h'] * scale
+    def pdfrw():
+        def adjust(page):
+            info = PageMerge().add(page)
+            x1, y1, x2, y2 = info.xobj_box
+            viewrect = (margin_x, margin_y, x2 - x1 - 2 * margin_x, y2 - y1 - 2 * margin_y)
+            page = PageMerge().add(page, viewrect=viewrect)
+            page[0].scale(scale)
+            return page.render()
 
-    # Number of pages in input document
-    page_count = reader.getNumPages()
+        reader = PdfReader(file_name)
+        writer = PdfWriter(output)
+        for i in list(range(0, len(reader.pages))):
+            writer.addpage(adjust(reader.pages[i]))
+        writer.trailer.Info = IndirectPdfDict(reader.Info or {})
+        writer.write()
 
-    for page_number in range(page_count):
-        wtrmrk = reader.getPage(page_number)
+    def pypdf3():
+        reader = PdfFileReader(file_name)
+        writer = PdfFileWriter()
+        dims = Info(file_name).dimensions
+        target_w = dims['w'] * scale
+        target_h = dims['h'] * scale
 
-        page = PageObject.createBlankPage(width=target_w, height=target_h)
-        page.mergeScaledTranslatedPage(wtrmrk, scale, margin_x, margin_y)
-        writer.addPage(page)
+        # Number of pages in input document
+        page_count = reader.getNumPages()
 
-    with open(output, "wb") as outputStream:
-        writer.write(outputStream)
+        for page_number in range(page_count):
+            wtrmrk = reader.getPage(page_number)
+
+            page = PageObject.createBlankPage(width=target_w, height=target_h)
+            page.mergeScaledTranslatedPage(wtrmrk, scale, margin_x, margin_y)
+            writer.addPage(page)
+
+        with open(output, "wb") as outputStream:
+            writer.write(outputStream)
+        return output
+
+    if method is 'pypdf3':
+        pypdf3()
+    else:
+        pdfrw()
     return output
