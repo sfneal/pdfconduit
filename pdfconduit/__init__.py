33a173f7efe110a019abee59576f115bd8100463
mrstephenneal <stephen.neal14@gmail.com>
2018-08-22 10:40:49 -0400

    Added flatten module to __init__.py

--- pdfconduit/__init__.py
+++ pdfconduit/__init__.py
@@ -6,10 +6,11 @@ from pdfconduit.rotate import rotate
 from pdfconduit.encrypt import Encrypt
 from pdfconduit.merge import Merge
 from pdfconduit.slice import slicer
+from pdfconduit.flatten import Flatten
 from pdfconduit.utils.gui import GUI
 
 
 __all__ = ["upscale", "rotate", "Encrypt", "Merge", "Watermark", "Label", "WatermarkAdd", "slicer",
-           "GUI", "Info"]
+           "GUI", "Info", "Flatten"]
 __version__ = __version__
 __author__ = __author__
