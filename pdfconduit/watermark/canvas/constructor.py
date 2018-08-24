f61d22e8e6276182a5d04f5a69eb9d6cce627ba7
mrstephenneal <stephen.neal14@gmail.com>
2018-08-24 12:58:10 -0400

    Added conditional statements to handle None values

--- pdfconduit/watermark/canvas/constructor.py
+++ pdfconduit/watermark/canvas/constructor.py
@@ -23,27 +23,49 @@ class CanvasConstructor:
         return self.obj, self.rotate
 
     def canvas(self):
-        self.obj.add(CanvasImg(os.path.join(IMAGE_DIRECTORY, self.image), opacity=self.opacity, x=0, y=153))
-        if self.copyright:
-            self.obj.add(CanvasStr('© copyright ' + str(datetime.now().year), size=16, y=-10, opacity=self.opacity))
-        if self.text2:
-            self.obj.add(CanvasStr(self.text1, opacity=self.opacity, size=40, y=-110))
-            self.obj.add(CanvasStr(self.text2, opacity=self.opacity, size=40, y=-160))
+        if self.image is not None:
+            self.obj.add(CanvasImg(os.path.join(IMAGE_DIRECTORY, self.image), opacity=self.opacity, x=0, y=153))
+            if self.copyright:
+                self.obj.add(CanvasStr('© copyright ' + str(datetime.now().year), size=16, y=-10, opacity=self.opacity))
+            if self.text2:
+                self.obj.add(CanvasStr(self.text1, opacity=self.opacity, size=40, y=-110))
+                self.obj.add(CanvasStr(self.text2, opacity=self.opacity, size=40, y=-160))
+            else:
+                self.obj.add(CanvasStr(self.text1, opacity=self.opacity, size=40, y=-125))
         else:
-            self.obj.add(CanvasStr(self.text1, opacity=self.opacity, size=40, y=-125))
+            if self.text2 and self.copyright:
+                self.obj.add(CanvasStr(self.text1, opacity=self.opacity, size=80, y=110))
+                self.obj.add(CanvasStr(self.text2, opacity=self.opacity, size=80, y=20))
+                self.obj.add(CanvasStr('© copyright ' + str(datetime.now().year), size=16, y=-30, opacity=self.opacity))
+            elif self.text2 and not self.copyright:
+                self.obj.add(CanvasStr(self.text1, opacity=self.opacity, size=80, y=90))
+                self.obj.add(CanvasStr(self.text2, opacity=self.opacity, size=80, y=0))
+            else:
+                self.obj.add(CanvasStr(self.text1, opacity=self.opacity, size=40, y=50))
         return self.objects
 
     def img(self):
         img = DrawPIL(tempdir=self.tempdir)
 
-        img.draw_img(os.path.join(IMAGE_DIRECTORY, self.image), x=0, y=50, opacity=self.opacity)
-        if self.copyright:
-            img.draw_text('© copyright ' + str(datetime.now().year), size=16, y='center')
-        if self.text2:
-            img.draw_text(self.text1, size=40, y=416, opacity=self.opacity)
-            img.draw_text(self.text2, size=40, y=466, opacity=self.opacity)
+        if self.image is not None:
+            img.draw_img(os.path.join(IMAGE_DIRECTORY, self.image), x=0, y=50, opacity=self.opacity)
+            if self.copyright:
+                img.draw_text('© copyright ' + str(datetime.now().year), size=16, y='center')
+            if self.text2:
+                img.draw_text(self.text1, size=40, y=416, opacity=self.opacity)
+                img.draw_text(self.text2, size=40, y=466, opacity=self.opacity)
+            else:
+                img.draw_text(self.text2, size=40, y=427, opacity=self.opacity)
         else:
-            img.draw_text(self.text2, size=40, y=427, opacity=self.opacity)
+            if self.text2 and self.copyright:
+                img.draw_text('© copyright ' + str(datetime.now().year), size=16, y=350)
+                img.draw_text(self.text1, size=80, y=106, opacity=self.opacity)
+                img.draw_text(self.text2, size=80, y=206, opacity=self.opacity)
+            elif self.text2 and not self.copyright:
+                img.draw_text(self.text1, size=80, y=106, opacity=self.opacity)
+                img.draw_text(self.text2, size=80, y=206, opacity=self.opacity)
+            else:
+                img.draw_text(self.text1, size=80, y='center', opacity=self.opacity)
 
         img.rotate(self.rotate)
         self.rotate = 0
