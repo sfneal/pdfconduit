7b0a83188eb8d37862db988c8bf358a7577649a7
mrstephenneal <stephen.neal14@gmail.com>
2018-08-24 14:18:12 -0400

    Moved gui.py to new gui module along with image directory functions

--- pdfconduit/conduit/utils/__init__.py
+++ pdfconduit/conduit/utils/__init__.py
@@ -1,6 +1,5 @@
 __all__ = ['set_destination', 'resource_path', 'add_suffix', 'open_window', 'overlay_pdfs', 'write_pdf', 'Info',
-           'get_directory', 'get_file', 'bundle_dir', 'Receipt', 'FONT', 'LETTER', 'IMAGE_DEFAULT',
-           'IMAGE_DIRECTORY', 'available_images', 'GUI']
+           'bundle_dir', 'Receipt', 'FONT', 'LETTER', 'IMAGE_DEFAULT']
 
 
 from .info import Info
@@ -8,5 +7,4 @@ from .path import set_destination, resource_path, add_suffix, bundle_dir
 from .view import open_window
 from .write import overlay_pdfs, write_pdf
 from .receipt import Receipt
-from .lib import FONT, LETTER, IMAGE_DIRECTORY, IMAGE_DEFAULT, available_images
-from .gui import get_directory, get_file, GUI
+from .lib import FONT, LETTER, IMAGE_DEFAULT
