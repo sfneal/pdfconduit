d35f22f9c678a9ede7c5b0165c66d0d1ddc0f356
mrstephenneal <stephen.neal14@gmail.com>
2018-08-30 15:51:04 -0400

    Improved test_all functionality

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,8 +1,17 @@
-import unittest
+import os
 
 
 def main():
-    unittest.main()
+    print(__file__)
+    directory = os.path.dirname(__file__)
+    test_scripts = [os.path.join(directory, i) for i in os.listdir(directory)
+                    if not i.startswith('.') and os.path.isfile(i) and i != 'test_all.py' and i != '__init__.py']
+
+    for i in test_scripts:
+        print(i)
+        m = str('tests.' + os.path.basename(i).strip('.py'))
+        command = 'python -m unittest ' + m
+        os.system(command)
 
 
 if __name__ == '__main__':
--- tests/test_all_pdfrw.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-def main():
-    unittest.main()
-
-
-if __name__ == '__main__':
-    main()
