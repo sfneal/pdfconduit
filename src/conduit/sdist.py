0c4b5546c9205cb6b5f9ebf0b31ecda26c86884d
mrstephenneal <stephen.neal14@gmail.com>
2018-08-30 15:52:15 -0400

    Modified conduit source distributor script to sync the utils directory

--- src/conduit/sdist.py
+++ src/conduit/sdist.py
@@ -11,6 +11,9 @@ DST_PATH1 = os.path.join(os.path.dirname(__file__), 'pdf', 'conduit')
 SRC_PATH2 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'pdfconduit'))
 DST_PATH2 = os.path.join(os.path.dirname(__file__), 'pdfconduit')
 
+SRC_PATH3 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'pdf', 'utils'))
+DST_PATH3 = os.path.join(os.path.dirname(__file__), 'pdf', 'utils')
+
 
 def sync_folder(source, destination):
     if os.path.exists(destination):
@@ -22,6 +25,7 @@ def main():
     print('Creating ~~conduit~~ source distribution')
     sync_folder(SRC_PATH1, DST_PATH1)
     sync_folder(SRC_PATH2, DST_PATH2)
+    sync_folder(SRC_PATH3, DST_PATH3)
     os.chdir(os.path.dirname(__file__))
     os.system('python setup.py sdist')
     sleep(1)
