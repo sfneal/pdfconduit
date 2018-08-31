c2eb8008ef4dd765a4d9f5e2ac4e9b3ac5be5129
mrstephenneal <stephen.neal14@gmail.com>
2018-08-31 14:10:16 -0400

    Added condition to avoid running test scripts with 'all' in filename

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,7 +5,7 @@ def main():
     print(__file__)
     directory = os.path.dirname(__file__)
     test_scripts = [os.path.join(directory, i) for i in os.listdir(directory)
-                    if not i.startswith('.') and os.path.isfile(i) and all not in i and i != '__init__.py']
+                    if not i.startswith('.') and os.path.isfile(i) and 'all' not in i and i != '__init__.py']
 
     for i in test_scripts:
         print(i)
