0efb9c7b8b4df28ac85f73cdf75e7df90fdfda5f
mrstephenneal <stephen.neal14@gmail.com>
2018-08-31 14:04:24 -0400

    Deleted test_watermark_pypdf3.py because of speed issues

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,8 +8,8 @@ from psutil import virtual_memory
 
 directory = os.path.join(os.path.dirname(__file__), 'data')
 # file_name = 'plan_l.pdf'
-# file_name = 'plan_p.pdf'
-file_name = 'article.pdf'
+file_name = 'plan_p.pdf'
+# file_name = 'article.pdf'
 # file_name = 'con docs2.pdf'
 # file_name = 'con docs2_sliced.pdf'
 pdf = os.path.join(directory, file_name)
