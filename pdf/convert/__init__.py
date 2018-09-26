ee0df54db100a9d7c28cef01af0212409f341823
mrstephenneal <stephen.neal14@gmail.com>
2018-09-26 11:32:46 -0400

    Added Flatten to __all__ declaration

--- pdf/convert/__init__.py
+++ pdf/convert/__init__.py
@@ -2,4 +2,4 @@ from pdf.convert.img2pdf import img2pdf, IMG2PDF
 from pdf.convert.pdf2img import pdf2img, PDF2IMG
 from pdf.convert.flatten import Flatten
 
-__all__ = ['img2pdf', 'pdf2img', 'IMG2PDF', 'PDF2IMG']
+__all__ = ['img2pdf', 'pdf2img', 'IMG2PDF', 'PDF2IMG', 'Flatten']
