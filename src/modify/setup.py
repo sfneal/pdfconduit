178435a1d0172376477f7b84512d631fb3b1729a
mrstephenneal <stephen.neal14@gmail.com>
2018-09-26 12:08:46 -0400

    Optimized install_requires

--- src/modify/setup.py
+++ src/modify/setup.py
@@ -19,13 +19,7 @@ setup(
     install_requires=[
         'PyPDF3>=1.0.1',
         'pdfrw',
-        'PyMuPDF>=1.13.20',
-        'Pillow',
-        'reportlab',
-        'PyBundle>=1.0.5',
-        'PySimpleGUI>=3.6.2',
-        'looptools',
-        'tqdm',
+        'pdfconduit-utils'
     ],
     name='pdfconduit-modify',
     version=verstr,
