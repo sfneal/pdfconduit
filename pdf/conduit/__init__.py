21da3a1f76b40bb7b98d8a3e7a89752ed3829d55
mrstephenneal <stephen.neal14@gmail.com>
2018-09-26 12:39:37 -0400

    Changed imports from relative to explicit

--- pdf/conduit/__init__.py
+++ pdf/conduit/__init__.py
@@ -1,6 +1,6 @@
-from .encrypt import Encrypt
-from .merge import Merge
-from .watermark import WatermarkAdd, Watermark, Label
+from pdf.conduit.encrypt import Encrypt
+from pdf.conduit.merge import Merge
+from pdf.conduit.watermark import WatermarkAdd, Watermark, Label
 
 
 __all__ = ["Encrypt", "Merge", "Watermark", "Label", "WatermarkAdd"]
