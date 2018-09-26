0c2ee2105a78e048df1933edbeecbcac833df3b1
mrstephenneal <stephen.neal14@gmail.com>
2018-09-26 12:53:16 -0400

    Added IMAGE_DIRECTORY and available_images to __all__ declaration

--- pdf/conduit/lib/__init__.py
+++ pdf/conduit/lib/__init__.py
@@ -1,4 +1,4 @@
 from pdf.conduit.lib.lib import FONT, LETTER, IMAGE_DEFAULT, IMAGE_DIRECTORY, available_images
 
 
-__all__ = ['FONT', 'LETTER', 'IMAGE_DEFAULT']
+__all__ = ['FONT', 'LETTER', 'IMAGE_DEFAULT', 'IMAGE_DIRECTORY', 'available_images']
