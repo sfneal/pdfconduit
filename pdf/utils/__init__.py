f7793d6eb07c3b4938e952080c796274481d7d98
mrstephenneal <stephen.neal14@gmail.com>
2018-09-26 12:48:30 -0400

    Added image_directory and available_images to __all__ declaration

--- pdf/utils/__init__.py
+++ pdf/utils/__init__.py
@@ -1,8 +1,9 @@
-__all__ = ['set_destination', 'add_suffix', 'open_window', 'overlay_pdfs', 'write_pdf', 'Info', 'Receipt']
+__all__ = ['set_destination', 'add_suffix', 'open_window', 'overlay_pdfs', 'write_pdf', 'Info', 'Receipt',
+           'IMAGE_DIRECTORY', 'available_images']
 
 
 from .info import Info
-from .path import set_destination, add_suffix
+from .path import set_destination, add_suffix, IMAGE_DIRECTORY, available_images
 from .view import open_window
 from .write import overlay_pdfs, write_pdf
 from .receipt import Receipt
