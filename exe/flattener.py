780200a007e16d192bb3e9ffbce2f92f61e133c6
mrstephenneal <stephen.neal14@gmail.com>
2018-09-28 12:25:55 -0400

    Rewrote flattener.py script and added flatten method to GUI class

--- exe/flattener.py
+++ exe/flattener.py
@@ -1,12 +1,8 @@
-from pdfconduit import Flatten
-from pdfconduit.utils.gui import get_directory
+from pdfconduit import GUI
 
 
 def main():
-    d = get_directory()
-
-    flat = Flatten(d, scale=1.5, progress_bar='gui').save(remove_temps=False)
-    print(flat)
+    GUI.flatten()
 
 
 if __name__ == '__main__':
