8731538f5ffd6add1136f6f68a7c9d4f833e9ac8
mrstephenneal <stephen.neal14@gmail.com>
2018-10-01 15:04:12 -0400

    Removed utils directory and updated sdist script

--- src/conduit/sdist.py
+++ src/conduit/sdist.py
@@ -11,9 +11,6 @@ DST_PATH1 = os.path.join(os.path.dirname(__file__), 'pdf', 'conduit')
 SRC_PATH2 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'pdfconduit'))
 DST_PATH2 = os.path.join(os.path.dirname(__file__), 'pdfconduit')
 
-SRC_PATH3 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'pdf', 'utils'))
-DST_PATH3 = os.path.join(os.path.dirname(__file__), 'pdf', 'utils')
-
 
 def sync_folder(source, destination):
     if os.path.exists(destination):
@@ -25,7 +22,6 @@ def main():
     print('Creating ~~conduit~~ source distribution')
     sync_folder(SRC_PATH1, DST_PATH1)
     sync_folder(SRC_PATH2, DST_PATH2)
-    sync_folder(SRC_PATH3, DST_PATH3)
     os.chdir(os.path.dirname(__file__))
     os.system('python setup.py sdist')
     sleep(1)
