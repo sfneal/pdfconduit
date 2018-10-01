4e613788b34a6eb3f0dfbc93a23e2bc6c9e20243
mrstephenneal <stephen.neal14@gmail.com>
2018-10-01 13:16:26 -0400

    Added modify module to src directory

---
+++ src/modify/pdf/modify/draw/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['DrawPDF', 'WatermarkDraw', 'DrawPIL', 'img_opacity']
+
+
+from pdf.modify.draw.pdf import DrawPDF, WatermarkDraw
+from pdf.modify.draw.image import DrawPIL, img_opacity
---
+++ src/modify/pdfconduit/__init__.py
@@ -0,0 +1,46 @@
+__all__ = []
+
+
+# Conduit installation
+try:
+    from pdf.conduit import Encrypt, Merge, Watermark, Label, WatermarkAdd
+    CONDUIT_INSTALL = True
+    __all__.extend(["Encrypt", "Merge", "Watermark", "Label", "WatermarkAdd"])
+except ImportError:
+    CONDUIT_INSTALL = False
+
+
+# Utils installation
+try:
+    from pdf.utils.info import Info
+    UTILS_INSTALL = True
+    __all__.extend(["Info"])
+except ImportError:
+    UTILS_INSTALL = False
+
+
+# GUI installation
+try:
+    from pdf.gui.gui import GUI
+    GUI_INSTALLED = True
+    __all__.extend(["GUI"])
+except ImportError:
+    GUI_INSTALLED = False
+
+
+# Modify installation
+try:
+    from pdf.transform import upscale, rotate, slicer
+    MODIFY_INSTALLED = True
+    __all__.extend(["slicer", "upscale", "rotate"])
+except ImportError:
+    MODIFY_INSTALLED = False
+
+
+# Convert installation
+try:
+    from pdf.convert.flatten import Flatten
+    MODIFY_INSTALLED = True
+    __all__.extend(["Flatten"])
+except ImportError:
+    MODIFY_INSTALLED = False
