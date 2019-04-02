cf089a48d36bd5dd4c9d07a5cba29fa059eb319c
mrstephenneal <stephen.neal14@gmail.com>
2019-04-02 10:10:36 -0400

    REFORMAT test_flatten with comments and timing

--- tests/test_flatten.py
+++ tests/test_flatten.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+from looptools import Timer
 from pdfconduit import Info, Flatten
 from tests import *
 
@@ -9,13 +10,21 @@ class TestFlatten(unittest.TestCase):
     def setUpClass(cls):
         cls.pdf_path = pdf_path
 
+    @Timer.decorator
     def test_flatten(self):
-        flat = Flatten(self.pdf_path, scale=1.0, suffix='flat_1x').save()
+        # Created a 'flattened' pdf file
+        flat = Flatten(self.pdf_path, suffix='flat_2x').save()
 
+        # Assert pdf file exists
         self.assertTrue(os.path.exists(flat))
+
+        # Assert there are the same number of pages in the 'original' and 'flattened' pdf
         self.assertEqual(Info(self.pdf_path).pages, Info(flat).pages)
+
+        # Confirm that PDF page sizes have not increased
         self.assertTrue(abs(Info(self.pdf_path).size[0] / Info(flat).size[0]) <= 1)
         self.assertTrue(abs(Info(self.pdf_path).size[1] / Info(flat).size[1]) <= 1)
+        return flat
 
 
 if __name__ == '__main__':
