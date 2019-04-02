eed29e09740f4a842d6865f04375556b9d2e750d
mrstephenneal <stephen.neal14@gmail.com>
2019-04-02 10:35:27 -0400

    ADD use of temporary directories to test_merge module

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,7 +1,6 @@
 import unittest
 import os
-import shutil
-import time
+from tempfile import TemporaryDirectory
 from pdfconduit import Info, Merge
 from tests import *
 
@@ -9,86 +8,35 @@ from tests import *
 class TestMerge(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # Destination directory
-        results = os.path.join(pdfs_dir, 'results')
-        if not os.path.isdir(results):
-            os.mkdir(results)
-        cls.dst = os.path.join(results, 'merge')
-
-        # Create destination if it does not exist
-        if not os.path.isdir(cls.dst):
-            os.mkdir(cls.dst)
-
-        # Log destination
-        cls.file_path = 'merge.csv'
-        cls.csv = os.path.join(os.path.dirname(__file__), 'log', cls.file_path)
-        cls.log = []
-
-        cls.files = []
-
-    @classmethod
-    def tearDownClass(cls):
-        write_log(cls.csv, cls.log)
+        cls.pdfs = [os.path.join(pdfs_dir, p) for p in ['article.pdf', 'charts.pdf', 'document.pdf', 'manual.pdf']]
 
     def setUp(self):
-        self.startTime = time.time()
+        self.temp = TemporaryDirectory()
 
     def tearDown(self):
-        t = time.time() - self.startTime
-        print("{0:15} --> {1}".format(' '.join(self.id().split('.')[-1].split('_')[2:]), t))
-
-        # Determine test result
-        if hasattr(self, '_outcome'):  # Python 3.4+
-            result = self.defaultTestResult()  # these 2 methods have no side effects
-            self._feedErrorsToResult(result, self._outcome.errors)
-        else:  # Python 3.2 - 3.3 or 3.0 - 3.1 and 2.7
-            result = getattr(self, '_outcomeForDoCleanups', self._resultForDoCleanups)
-        error = self.list2reason(result.errors)
-        failure = self.list2reason(result.failures)
-        ok = not error and not failure
-
-        # demo:   report short info immediately (not important)
-        if not ok:
-            typ, text = ('ERROR', error) if error else ('FAIL', failure)
-        else:
-            typ = 'PASS'
-
-        # Log dump
-        rows, file_path = dump_log(test_case=self.id().split('.'), time=t, result=typ)
-        self.log.append(rows)
-        self.file_path = file_path
-
-        # Move each file into results folder
-        for i in self.files:
-            source = i
-            target = os.path.join(self.dst, str(os.path.basename(i)))
-            try:
-                shutil.move(source, target)
-                self.files.remove(i)
-            except FileNotFoundError:
-                pass
-
-    def list2reason(self, exc_list):
-        if exc_list and exc_list[-1][0] is self:
-            return exc_list[-1][1]
+        self.temp.cleanup()
 
     def test_merge_pypdf3(self):
-        pdfs = [os.path.join(pdfs_dir, p) for p in ['article.pdf', 'charts.pdf', 'document.pdf', 'manual.pdf']]
-        m = Merge(pdfs, output_name='merged_pypdf3', method='pypdf3')
+        """Merge multiple PDF files into a single PDF using the `PyPDF3` library."""
+        merged = Merge(self.pdfs, output_name='merged_pypdf3', output_dir=self.temp.name, method='pypdf3')
 
-        self.assertTrue(os.path.exists(m.file))
-        self.assertEqual(sum([Info(pdf).pages for pdf in pdfs]), Info(m.file).pages)
+        # Assert merged file exists
+        self.assertTrue(os.path.exists(merged.file))
 
-        self.files.append(m.file)
+        # Assert sum of pages in original pdf files equals sum of pages in merged pdf
+        self.assertEqual(sum([Info(pdf).pages for pdf in self.pdfs]), Info(merged.file).pages)
+        return merged
 
     def test_merge_pdfrw(self):
-        pdfs = [os.path.join(pdfs_dir, p) for p in ['article.pdf', 'charts.pdf', 'document.pdf', 'manual.pdf']]
-        m = Merge(pdfs, output_name='merged_pdfrw', method='pdfrw')
+        """Merge multiple PDF files into a single PDF using the `pdfrw` library."""
+        merged = Merge(self.pdfs, output_name='merged_pdfrw', output_dir=self.temp.name, method='pdfrw')
 
-        self.assertTrue(os.path.exists(m.file))
-        self.assertEqual(sum([Info(pdf).pages for pdf in pdfs]), Info(m.file).pages)
+        # Assert merged file exists
+        self.assertTrue(os.path.exists(merged.file))
 
-        self.files.append(m.file)
+        # Assert sum of pages in original pdf files equals sum of pages in merged pdf
+        self.assertEqual(sum([Info(pdf).pages for pdf in self.pdfs]), Info(merged.file).pages)
+        return merged
 
 
 if __name__ == '__main__':
