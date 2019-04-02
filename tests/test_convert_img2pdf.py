b42c83a5ab86a346171cf5fee7c8396d7fdc93f2
mrstephenneal <stephen.neal14@gmail.com>
2019-04-02 11:57:45 -0400

    ADD test_convert_img2pdf test methods

--- tests/test_convert_img2pdf.py
+++ tests/test_convert_img2pdf.py
@@ -1,7 +1,8 @@
 import unittest
 import os
+from tempfile import TemporaryDirectory
 from looptools import Timer
-from pdfconduit import Info, Flatten
+from pdf.convert import IMG2PDF
 from tests import *
 
 
@@ -9,28 +10,24 @@ class TestConvertImg2Pdf(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.img_path = img_path
-        cls.flat = None
+        cls.pdf = None
+
+    def setUp(self):
+        self.temp = TemporaryDirectory()
 
     def tearDown(self):
-        if os.path.exists(self.flat):
-            os.remove(self.flat)
+        if os.path.exists(self.pdf):
+            os.remove(self.pdf)
 
     @Timer.decorator
-    def test_flatten(self):
+    def test_convert_img2pdf(self):
         """Create a 'flattened' pdf file without layers."""
-        flat = Flatten(self.pdf_path, suffix='flat').save()
+        pdf = IMG2PDF([self.img_path], destination=test_data_dir, tempdir=self.temp.name).save()
 
         # Assert pdf file exists
-        self.assertTrue(os.path.exists(flat))
-
-        # Assert there are the same number of pages in the 'original' and 'flattened' pdf
-        self.assertEqual(Info(self.pdf_path).pages, Info(flat).pages)
-
-        # Confirm that PDF page sizes have not increased
-        self.assertTrue(abs(Info(self.pdf_path).size[0] / Info(flat).size[0]) <= 1)
-        self.assertTrue(abs(Info(self.pdf_path).size[1] / Info(flat).size[1]) <= 1)
-        self.flat = flat
-        return flat
+        self.assertTrue(os.path.exists(pdf))
+        self.pdf = pdf
+        return pdf
 
 
 if __name__ == '__main__':
