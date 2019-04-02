2eff2e7ebd264f54ca414d4fdb21bf30f479cfff
mrstephenneal <stephen.neal14@gmail.com>
2019-04-02 15:59:19 -0400

    CUT gui related test methods because they are functionally identical

--- tests/test_convert_flatten.py
+++ tests/test_convert_flatten.py
@@ -32,40 +32,6 @@ class TestConvertFlatten(unittest.TestCase):
         self.flat = flat
         return flat
 
-    @Timer.decorator
-    def test_convert_flatten_gui(self):
-        """Create a 'flattened' pdf file without layers and monitor using a PySimpleGUI progress bar."""
-        flat = Flatten(self.pdf_path, suffix='flat_gui', progress_bar='gui').save()
-
-        # Assert pdf file exists
-        self.assertTrue(os.path.exists(flat))
-
-        # Assert there are the same number of pages in the 'original' and 'flattened' pdf
-        self.assertEqual(Info(self.pdf_path).pages, Info(flat).pages)
-
-        # Confirm that PDF page sizes have not increased
-        self.assertTrue(abs(Info(self.pdf_path).size[0] / Info(flat).size[0]) <= 1)
-        self.assertTrue(abs(Info(self.pdf_path).size[1] / Info(flat).size[1]) <= 1)
-        self.flat = flat
-        return flat
-
-    @Timer.decorator
-    def test_convert_flatten_tqdm(self):
-        """Create a 'flattened' pdf file without layers and monitor using a tqdm progress bar."""
-        flat = Flatten(self.pdf_path, suffix='flat_tqdm', progress_bar='tqdm').save()
-
-        # Assert pdf file exists
-        self.assertTrue(os.path.exists(flat))
-
-        # Assert there are the same number of pages in the 'original' and 'flattened' pdf
-        self.assertEqual(Info(self.pdf_path).pages, Info(flat).pages)
-
-        # Confirm that PDF page sizes have not increased
-        self.assertTrue(abs(Info(self.pdf_path).size[0] / Info(flat).size[0]) <= 1)
-        self.assertTrue(abs(Info(self.pdf_path).size[1] / Info(flat).size[1]) <= 1)
-        self.flat = flat
-        return flat
-
 
 if __name__ == '__main__':
     unittest.main()
