62cb8f5ee2ffe78e26c1b3103793199c5637f6af
sfneal <stephen.neal14@gmail.com>
2024-06-28 18:40:40 -0400

    REFACTOR test_conduit_encrypt.py to use custom assertion methods

--- tests/test_conduit_encrypt.py
+++ tests/test_conduit_encrypt.py
@@ -25,33 +25,6 @@ class TestEncrypt(unittest.TestCase):
             os.remove(self.temp.name)
 
     @Timer.decorator
-    def test_encrypt_printing(self):
-        """Encrypt a PDF file and allow users to print."""
-        encrypted = Encrypt(
-            self.pdf_path,
-            self.user_pw,
-            self.owner_pw,
-            allow_printing=True,
-            output=self.temp.name,
-            suffix='allow_printing',
-        )
-
-        # Encrypted pdf security info
-        security = Info(encrypted.output, self.user_pw).security
-
-        # Assert that pdf file exists
-        self.assertTrue(os.path.exists(encrypted.output))
-
-        # Assert that pdf file is now encrypted
-        self.assertTrue(Info(encrypted.output, self.user_pw).encrypted)
-
-        # Assert encryption bit size is 128
-        self.assertEqual(security['/Length'], 128)
-
-        # Assert pdf security value is -1852
-        self.assertEqual(security['/P'], -1852)
-
-    @Timer.decorator
     def test_encrypt_128bit(self):
         """Encrypt PDF file with 128bit encryption."""
         encrypted = Encrypt(self.pdf_path,
@@ -61,23 +34,12 @@ class TestEncrypt(unittest.TestCase):
                             bit128=True,
                             suffix='128bit')
 
-        # Encrypted pdf security info
-        security = Info(encrypted.output, self.user_pw).security
+        security = self._getPdfSecurity(encrypted)
 
-        # Assert that pdf file exists
-        self.assertTrue(os.path.exists(encrypted.output))
-
-        # Assert that pdf file is now encrypted
-        self.assertTrue(Info(encrypted.output, self.user_pw).encrypted)
-
-        # Assert encryption bit size is 128
-        self.assertEqual(security['/Length'], 128)
-
-        # Assert pdf security value is -1852
-        self.assertEqual(security['/P'], -1852)
-
-        # Assert standard security handler revision is 3
-        self.assertEqual(security['/R'], 3)
+        self.assertPdfExists(encrypted)
+        self.assertEncrypted(encrypted)
+        self.assert128BitEncryption(security)
+        self.assertSecurityValue(security, -1852)
 
     @Timer.decorator
     def test_encrypt_40bit(self):
@@ -89,23 +51,32 @@ class TestEncrypt(unittest.TestCase):
                             bit128=False,
                             suffix='40bit')
 
-        # Encrypted pdf security info
-        security = Info(encrypted.output, self.user_pw).security
+        security = self._getPdfSecurity(encrypted)
 
-        # Assert that pdf file exists
-        self.assertTrue(os.path.exists(encrypted.output))
+        self.assertPdfExists(encrypted)
+        self.assertEncrypted(encrypted)
+        self.assert40BitEncryption(security)
+        self.assertSecurityValue(security, -1852)
 
-        # Assert that pdf file is now encrypted
-        self.assertTrue(Info(encrypted.output, self.user_pw).encrypted)
+    @Timer.decorator
+    def test_encrypt_printing(self):
+        """Encrypt a PDF file and allow users to print."""
+        # todo: allow and don't allow tests
+        encrypted = Encrypt(
+            self.pdf_path,
+            self.user_pw,
+            self.owner_pw,
+            allow_printing=True,
+            output=self.temp.name,
+            suffix='allow_printing',
+        )
 
-        # Confirm length (128 for 128bit encryption) is missing
-        self.assertFalse('/Length' in security)
+        security = self._getPdfSecurity(encrypted)
 
-        # Assert pdf security value is -1852
-        self.assertEqual(security['/P'], -1852)
-
-        # Assert standard security handler revision is 2
-        self.assertEqual(security['/R'], 2)
+        self.assertPdfExists(encrypted)
+        self.assertEncrypted(encrypted)
+        self.assert128BitEncryption(security)
+        self.assertSecurityValue(security, -1852)
 
     @Timer.decorator
     def test_encrypt_128bit_allow_commenting(self):
@@ -119,17 +90,11 @@ class TestEncrypt(unittest.TestCase):
                             bit128=True,
                             suffix='128bit_allow_commenting')
 
-        # Encrypted pdf security info
-        security = Info(encrypted.output, self.user_pw).security
+        security = self._getPdfSecurity(encrypted)
 
-        # Assert that pdf file exists
-        self.assertTrue(os.path.exists(encrypted.output))
-
-        # Assert that pdf file is now encrypted
-        self.assertTrue(Info(encrypted.output, self.user_pw).encrypted)
-
-        # Assert pdf security value is -800
-        self.assertEqual(security['/P'], -800)
+        self.assertPdfExists(encrypted)
+        self.assertEncrypted(encrypted)
+        self.assertSecurityValue(security, -800)
 
     @Timer.decorator
     def test_encrypt_128bit_allow_printing_and_commenting(self):
@@ -143,23 +108,12 @@ class TestEncrypt(unittest.TestCase):
                             bit128=True,
                             suffix='128bit_allow_printing_and_commenting')
 
-        # Encrypted pdf security info
-        security = Info(encrypted.output, self.user_pw).security
+        security = self._getPdfSecurity(encrypted)
 
-        # Assert that pdf file exists
-        self.assertTrue(os.path.exists(encrypted.output))
-
-        # Assert that pdf file is now encrypted
-        self.assertTrue(Info(encrypted.output, self.user_pw).encrypted)
-
-        # Assert pdf security value is -1500
-        self.assertEqual(security['/P'], -1500)
-
-        # Assert encryption bit size is 128
-        self.assertEqual(security['/Length'], 128)
-
-        # Assert standard security handler revision is 3
-        self.assertEqual(security['/R'], 3)
+        self.assertPdfExists(encrypted)
+        self.assertEncrypted(encrypted)
+        self.assert128BitEncryption(security)
+        self.assertSecurityValue(security, -1500)
 
     @Timer.decorator
     def test_encrypt_40bit_allow_commenting(self):
@@ -173,23 +127,12 @@ class TestEncrypt(unittest.TestCase):
                             bit128=False,
                             suffix='40bit_allow_commenting')
 
-        # Encrypted pdf security info
-        security = Info(encrypted.output, self.user_pw).security
+        security = self._getPdfSecurity(encrypted)
 
-        # Assert that pdf file exists
-        self.assertTrue(os.path.exists(encrypted.output))
-
-        # Assert that pdf file is now encrypted
-        self.assertTrue(Info(encrypted.output, self.user_pw).encrypted)
-
-        # Assert pdf security value is -800
-        self.assertEqual(security['/P'], -800)
-
-        # Confirm length (128 for 128bit encryption) is missing
-        self.assertFalse('/Length' in security)
-
-        # Assert standard security handler revision is 2
-        self.assertEqual(security['/R'], 2)
+        self.assertPdfExists(encrypted)
+        self.assertEncrypted(encrypted)
+        self.assert40BitEncryption(security)
+        self.assertSecurityValue(security, -800)
 
     @Timer.decorator
     def test_encrypt_40bit_allow_printing_and_commenting(self):
@@ -203,23 +146,12 @@ class TestEncrypt(unittest.TestCase):
                             bit128=False,
                             suffix='40bit_allow_printing_and_commenting')
 
-        # Encrypted pdf security info
-        security = Info(encrypted.output, self.user_pw).security
+        security = self._getPdfSecurity(encrypted)
 
-        # Assert that pdf file exists
-        self.assertTrue(os.path.exists(encrypted.output))
-
-        # Assert that pdf file is now encrypted
-        self.assertTrue(Info(encrypted.output, self.user_pw).encrypted)
-
-        # Assert pdf security value is -1500
-        self.assertEqual(security['/P'], -1500)
-
-        # Confirm length (128 for 128bit encryption) is missing
-        self.assertFalse('/Length' in security)
-
-        # Assert standard security handler revision is 2
-        self.assertEqual(security['/R'], 2)
+        self.assertPdfExists(encrypted)
+        self.assertEncrypted(encrypted)
+        self.assert40BitEncryption(security)
+        self.assertSecurityValue(security, -1500)
 
     @Timer.decorator
     def test_password_byte_string(self):
@@ -232,23 +164,12 @@ class TestEncrypt(unittest.TestCase):
             suffix='byte_string',
         )
 
-        # Encrypted pdf security info
-        security = Info(encrypted.output, self.user_pw).security
+        security = self._getPdfSecurity(encrypted)
 
-        # Assert that pdf file exists
-        self.assertTrue(os.path.exists(encrypted.output))
-
-        # Assert that pdf file is now encrypted
-        self.assertTrue(Info(encrypted.output, self.user_pw).encrypted)
-
-        # Assert encryption bit size is 128
-        self.assertEqual(security['/Length'], 128)
-
-        # Assert pdf security value is -1852
-        self.assertEqual(security['/P'], -1852)
-
-        # Assert standard security handler revision is 3
-        self.assertEqual(security['/R'], 3)
+        self.assertPdfExists(encrypted)
+        self.assertEncrypted(encrypted)
+        self.assert128BitEncryption(security)
+        self.assertSecurityValue(security, -1852)
 
         # Assert user & owner password byte string is correct
         self.assertEqual(
@@ -256,8 +177,33 @@ class TestEncrypt(unittest.TestCase):
             b'\xd0H\xd1R\x9eS]\x18\x84\xcd8V6{\x18KJ\x90\xdf\x01\xe67\xd1n\xca\x06[\xafNd\x90\x0b'
         )
 
+    def _getPdfSecurity(self, encrypted):
+        return Info(encrypted.output, self.user_pw).security
 
+    def assertPdfExists(self, encrypted):
+        # Assert that pdf file exists
+        self.assertTrue(os.path.exists(encrypted.output))
 
+    def assertEncrypted(self, pdf):
+        self.assertTrue(Info(pdf.output, self.user_pw).encrypted)
+
+    def assert128BitEncryption(self, security):
+        self.assertTrue('/Length' in security)
+        self.assertIsInstance(security['/Length'], int)
+        self.assertEqual(security['/Length'], 128)
+
+        # Assert standard security handler revision is 3
+        self.assertEqual(security['/R'], 3)
+
+    def assert40BitEncryption(self, security):
+        self.assertFalse('/Length' in security)
+
+        # Assert standard security handler revision is 2
+        self.assertEqual(security['/R'], 2)
+
+    def assertSecurityValue(self, security, expected):
+        self.assertTrue('/P' in security)
+        self.assertEqual(security['/P'], expected)
 
 
 if __name__ == '__main__':
