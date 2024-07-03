646208d1a674718ca898e26890f403f3e64687be
sfneal <stephen.neal14@gmail.com>
2024-07-03 14:38:23 -0400

    ADD pypdf functionality to info.py for getting pdf info using pypdf instead of pypdf3

--- pdfconduit/utils/info.py
+++ pdfconduit/utils/info.py
@@ -1,13 +1,18 @@
 # Retrieve information about a PDF document
 from PyPDF3 import PdfFileReader
+from pdfconduit.utils.read import pypdf_reader
 
 
 class Info:
-    def __init__(self, path, password=None, prompt=True):
-        self.pdf = self._reader(path, password, prompt)
+    def __init__(self, path, password=None, prompt=True, use_pypdf=False):
+        self.use_pypdf = use_pypdf
+        if use_pypdf:
+            self.pdf = pypdf_reader(path, password)
+        else:
+            self.pdf = self._pypdf3_reader(path, password, prompt)
 
     @staticmethod
-    def _reader(path, password, prompt):
+    def _pypdf3_reader(path, password, prompt):
         """Read PDF and decrypt if encrypted."""
         pdf = PdfFileReader(path) if not isinstance(path, PdfFileReader) else path
         # Check that PDF is encrypted
@@ -32,6 +37,8 @@ class Info:
     @property
     def encrypted(self):
         """Check weather a PDF is encrypted"""
+        if self.use_pypdf:
+            return self.pdf.is_encrypted
         return True if self.pdf.isEncrypted else False
 
     @property
@@ -47,7 +54,7 @@ class Info:
     @property
     def metadata(self):
         """Retrieve PDF metadata"""
-        return self.pdf.getDocumentInfo()
+        return self.pdf.metadata
 
     def resources(self):
         """Retrieve contents of each page of PDF"""
@@ -56,6 +63,8 @@ class Info:
     @property
     def security(self):
         """Print security object information for a pdf document"""
+        if self.use_pypdf:
+            return {k: v for i in self.pdf.resolved_objects.items() for k, v in i[1].items()}
         return {k: v for i in self.pdf.resolvedObjects.items() for k, v in i[1].items()}
 
     @property
