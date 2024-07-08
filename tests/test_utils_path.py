c4c4656f32637a9dcc2915222e3a4dc2cddabc08
sfneal <stephen.neal14@gmail.com>
2024-07-08 12:43:51 -0400

    MAKE test_utils_path.py for testing `add_suffix()` function

---
+++ tests/test_utils_path.py
@@ -0,0 +1,53 @@
+import os.path
+import unittest
+
+from looptools import Timer
+
+from pdfconduit.utils import add_suffix, set_destination
+from tests import *
+
+
+class TestPath(unittest.TestCase):
+    @Timer.decorator
+    def test_add_suffix(self):
+        file_path = test_data_path('article.pdf')
+        with_suffix = add_suffix(file_path)
+
+        self.assertIsInstance(with_suffix, str)
+        self.assertEqual(with_suffix, os.path.join(os.path.dirname(file_path), 'article_modified.pdf'))
+
+    @Timer.decorator
+    def test_add_suffix_suffix(self):
+        file_path = test_data_path('article.pdf')
+        with_suffix = add_suffix(file_path, 'new')
+
+        self.assertIsInstance(with_suffix, str)
+        self.assertEqual(with_suffix, os.path.join(os.path.dirname(file_path), 'article_new.pdf'))
+
+    @Timer.decorator
+    def test_add_suffix_suffix_sep(self):
+        file_path = test_data_path('article.pdf')
+        with_suffix = add_suffix(file_path, 'old', '-')
+
+        self.assertIsInstance(with_suffix, str)
+        self.assertEqual(with_suffix, os.path.join(os.path.dirname(file_path), 'article-old.pdf'))
+
+    @Timer.decorator
+    def test_add_suffix_suffix_sep_ext(self):
+        file_path = test_data_path('article.pdf')
+        with_suffix = add_suffix(file_path, 'backup', '-', 'zip')
+
+        self.assertIsInstance(with_suffix, str)
+        self.assertEqual(with_suffix, os.path.join(os.path.dirname(file_path), 'article-backup.zip'))
+
+    @Timer.decorator
+    def test_set_destination(self):
+        file_path = test_data_path('charts.pdf')
+        destination = set_destination(file_path, '')
+
+        self.assertIsInstance(destination, str)
+        self.assertEqual(destination, os.path.join(os.path.dirname(file_path), 'article_modified.pdf'))
+
+
+if __name__ == '__main__':
+    unittest.main()
