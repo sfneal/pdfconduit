aef041582126706ded4f300c61349bb9f5565a82
sfneal <stephen.neal14@gmail.com>
2024-07-09 14:13:17 -0400

    FIX issue with get_version.sh script & deploy action

--- .github/scripts/get_version.sh
+++ .github/scripts/get_version.sh
@@ -6,6 +6,6 @@ set -e
 python - << EOF
 import os
 with open(os.path.join(os.path.dirname(__file__), 'pdfconduit', '_version.py'), 'rb') as fp:
-    print(fp.read().decode('utf8').split('=')[1].strip(" \n'"))
+    print(fp.read().decode('utf8').split('=')[1].strip(" \n'").replace('"', ''))
 
 EOF
