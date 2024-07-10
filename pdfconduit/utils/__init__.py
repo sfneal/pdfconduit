1f6c5314088c1f9c901b1fdb53c21c22dedf5fa5
sfneal <stephen.neal14@gmail.com>
2024-07-10 12:59:30 -0400

    FIX imports

--- pdfconduit/utils/__init__.py
+++ pdfconduit/utils/__init__.py
@@ -1,6 +1,6 @@
 from pdfconduit.utils.info import Info
 from pdfconduit.utils.path import set_destination, add_suffix
-from pdfconduit.utils.read import pypdf3_reader
+from pdfconduit.utils.read import pypdf_reader
 from pdfconduit.utils.receipt import Receipt
 from pdfconduit.utils.view import open_window
 from pdfconduit.utils.write import overlay_pdfs, write_pdf
@@ -13,5 +13,5 @@ __all__ = [
     "write_pdf",
     "Info",
     "Receipt",
-    "pypdf3_reader",
+    "pypdf_reader",
 ]
