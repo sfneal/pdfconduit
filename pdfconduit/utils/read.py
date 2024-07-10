af14ceec89c0ca07b9f853e50bda0ce84080041e
sfneal <stephen.neal14@gmail.com>
2024-07-10 12:59:24 -0400

    CUT `pypdf3_reader()` func from read.py  - cut pypdf3 reader tests

--- pdfconduit/utils/read.py
+++ pdfconduit/utils/read.py
@@ -1,21 +1,4 @@
-from PyPDF3 import PdfFileReader
 from pypdf import PdfReader
-
-
-def pypdf3_reader(pdf, decrypt=None) -> PdfFileReader:
-    """
-    Retrieve a PdfFileReader object that has been decrypted if a password is specified.
-
-    :param pdf: PDF document to read
-    :param decrypt: Owner password to decrypt pdf
-    :return: PdfFileReader object
-    """
-    if decrypt:
-        reader = PdfFileReader(pdf)
-        reader.decrypt(decrypt)
-        return reader
-    else:
-        return PdfFileReader(pdf)
 
 
 def pypdf_reader(pdf, decrypt=None) -> PdfReader:
