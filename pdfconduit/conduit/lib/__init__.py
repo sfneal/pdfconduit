e3ec76bb2d57dfe232943501d6a6ab24f0659565
sfneal <stephen.neal14@gmail.com>
2024-07-10 15:26:05 -0400

    ADD type hinting & refactor variable names

--- pdfconduit/conduit/lib/__init__.py
+++ pdfconduit/conduit/lib/__init__.py
@@ -1,25 +1,26 @@
 import os
+from typing import List
 
 from PyBundle import bundle_dir, resource_path
 
 
-def _image_directory():
+def _image_directory() -> str:
     directory = os.path.join(bundle_dir(), "img")
     if os.path.exists(directory):
         return directory
     else:
-        print(directory, "can not be found")
+        raise Exception("No image directory found")
 
 
 IMAGE_DIRECTORY = _image_directory()
 
 
-def available_images():
-    imgs = [i for i in os.listdir(IMAGE_DIRECTORY) if not i.startswith(".")]
-    if len(imgs) > 0:
-        return sorted(imgs, reverse=True)
+def available_images() -> List[str]:
+    images = [i for i in os.listdir(IMAGE_DIRECTORY) if not i.startswith(".")]
+    if len(images) > 0:
+        return sorted(images, reverse=True)
     else:
-        return ["Add images..."]
+        return []
 
 
 IMAGE_DEFAULT = resource_path("watermark.png")
