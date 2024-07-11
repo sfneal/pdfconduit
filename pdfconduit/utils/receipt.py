2640f5ce1d601d9de772bc5197bf98f834795bb3
sfneal <stephen.neal14@gmail.com>
2024-07-11 13:48:08 -0400

    ADD type hinting to receipt.py

--- pdfconduit/utils/receipt.py
+++ pdfconduit/utils/receipt.py
@@ -1,10 +1,11 @@
 import os
 from datetime import datetime
+from typing import Self, Any
 
 
 class Receipt:
     # todo: refactor to write to logs
-    def __init__(self, use=True):
+    def __init__(self, use: bool=True):
         self.dst = None
         self.use = use
         self.items = []
@@ -12,13 +13,13 @@ class Receipt:
 
         self.add("PDF Watermarker", datetime.now().strftime("%Y-%m-%d %H:%M"))
 
-    def set_dst(self, doc, file_name="watermark receipt.txt"):
+    def set_dst(self, doc: str, file_name: str="watermark receipt.txt") -> Self:
         self.dst = os.path.join(os.path.dirname(doc), file_name)
         self.add("Directory", os.path.dirname(doc))
         self.add("PDF", os.path.basename(doc))
         return self
 
-    def add(self, key, value):
+    def add(self, key: str, value: Any):
         message = str("{0:20}--> {1}".format(key, value))
         if self.use:
             self._print(message)
@@ -28,9 +29,7 @@ class Receipt:
         exists = os.path.isfile(self.dst)
         with open(self.dst, "a") as f:
             if exists:
-                f.write(
-                    "*******************************************************************\n"
-                )
+                f.write("*******************************************************************\n")
 
             for item in self.items:
                 f.write(item + "\n")
