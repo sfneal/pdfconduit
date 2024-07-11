2773a21a98fc9e57f9a46cdf0e8a5620587544d7
sfneal <stephen.neal14@gmail.com>
2024-07-11 15:43:52 -0400

    REFACTOR pdfconduit.modify module imports

--- pdfconduit/modify/__init__.py
+++ pdfconduit/modify/__init__.py
@@ -1,5 +1,9 @@
 from reportlab.lib.pagesizes import letter
 
+from pdfconduit.modify.canvas.constructor import CanvasConstructor
+from pdfconduit.modify.canvas.objects import CanvasImg, CanvasStr, CanvasObjects
+from pdfconduit.modify.draw.pdf import DrawPDF, WatermarkDraw
+
 LETTER = letter[1], letter[0]
 
-__all__ = ["LETTER"]
+__all__ = ["LETTER", "CanvasConstructor", "CanvasImg", "CanvasStr", "CanvasObjects", "DrawPDF", "WatermarkDraw"]
