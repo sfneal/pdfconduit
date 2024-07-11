35b60dd3032d5f27c9d30c9eae7e59f96a75a28a
sfneal <stephen.neal14@gmail.com>
2024-07-11 15:49:31 -0400

    Revert "REFACTOR pdfconduit.modify module imports"
    This reverts commit 2773a21a98fc9e57f9a46cdf0e8a5620587544d7.

--- pdfconduit/modify/__init__.py
+++ pdfconduit/modify/__init__.py
@@ -1,9 +1,5 @@
 from reportlab.lib.pagesizes import letter
 
-from pdfconduit.modify.canvas.constructor import CanvasConstructor
-from pdfconduit.modify.canvas.objects import CanvasImg, CanvasStr, CanvasObjects
-from pdfconduit.modify.draw.pdf import DrawPDF, WatermarkDraw
-
 LETTER = letter[1], letter[0]
 
-__all__ = ["LETTER", "CanvasConstructor", "CanvasImg", "CanvasStr", "CanvasObjects", "DrawPDF", "WatermarkDraw"]
+__all__ = ["LETTER"]
