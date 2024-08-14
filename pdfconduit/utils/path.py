dfffe94a76426de37e099230d8b52bc2b792740e
sfneal <stephen.neal14@gmail.com>
2024-08-14 12:02:54 -0400

    CUT non-pathlib implementation of `add_suffix()` function

--- pdfconduit/utils/path.py
+++ pdfconduit/utils/path.py
@@ -1,29 +1,12 @@
 # Set directory paths and file names
 import os
-import sys
 from pathlib import Path
 from typing import Optional
 
-if "pathlib" in sys.modules:
 
-    def _add_suffix(file_path: str, suffix: str, sep: str, ext: str):
-        p = Path(file_path)
-        _ext = p.suffix if ext is None else str("." + ext.strip("."))
-        out = p.stem + sep + suffix + _ext  # p.suffix is file extension
-        return os.path.join(os.path.dirname(file_path), out)
-
-else:
-
-    def _add_suffix(file_path: str, suffix: str, sep: str, ext: str):
-        split = os.path.basename(file_path).rsplit(".", 1)
-        ext = split[1] if ext is None else str("." + ext.strip("."))
-        return os.path.join(
-            os.path.dirname(file_path), split[0] + sep + suffix + "." + ext
-        )
-
-
-def add_suffix(
-    file_path: str, suffix: str = "modified", sep: str = "_", ext: Optional[str] = None
-):
+def add_suffix(file_path: str, suffix: str = "modified", sep: str = "_", ext: Optional[str] = None):
     """Adds suffix to a file name seperated by an underscore and returns file path."""
-    return _add_suffix(file_path, suffix, sep, ext)
+    p = Path(file_path)
+    _ext = p.suffix if ext is None else str("." + ext.strip("."))
+    out = p.stem + sep + suffix + _ext  # p.suffix is file extension
+    return os.path.join(os.path.dirname(file_path), out)
