13954a11b4b8986b23e50100aa9b37b6b70cec76
sfneal <stephen.neal14@gmail.com>
2024-08-21 13:47:59 -0400

    FIX bug with named temp file suffix/extension

--- pdfconduit/internals/base.py
+++ pdfconduit/internals/base.py
@@ -134,7 +134,7 @@ class BaseConduit(ABC):
 
     def set_output_temp(self, tempdir: Optional[TemporaryDirectory] = None, suffix: str = '') -> Self:
         self._tempdir = tempdir if tempdir else TemporaryDirectory(prefix='pdfconduit_', delete=False)
-        self._tempfile = NamedTemporaryFile(suffix=suffix + 'pdf', dir=self._tempdir.name, delete=False, delete_on_close=False)
+        self._tempfile = NamedTemporaryFile(suffix='_' + suffix.replace('_', '') + '.pdf', dir=self._tempdir.name, delete=False, delete_on_close=False)
         return self.set_output(self._tempfile.name)
 
     def _set_default_output(self, suffix: str) -> None:
