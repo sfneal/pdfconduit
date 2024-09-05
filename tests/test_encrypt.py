a85f3426e8b09ffd2eacd162f5bd7a384dc7728b
sfneal <stephen.neal14@gmail.com>
2024-09-05 12:41:55 -0400

    ADD use of `assertSuffixIsCorrect()` assertion method in various test suites

--- tests/test_encrypt.py
+++ tests/test_encrypt.py
@@ -152,6 +152,8 @@ class TestEncryption(EncryptionTestCase):
             can_modify=encryption.allow_commenting,
         )
 
+        self.assertSuffixIsCorrect(self.conduit.output, 'encrypted')
+
     def test_password_byte_string(self):
         self.conduit.encrypt(
             Encryption(user_pw="baz", owner_pw="foo", algo=Algorithms.RC4_128)
